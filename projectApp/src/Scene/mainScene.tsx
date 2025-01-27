9b34fe0590be8331c5d73fb3a7098fdb3b38232e
RandallSungcang <randallsungcang3@gmail.com>
2025-01-27 17:52:02 -0600

    Cleaning up

--- projectApp/src/Scene/mainScene.tsx
+++ projectApp/src/Scene/mainScene.tsx
@@ -1,7 +1,5 @@
 import { useFrame } from '@react-three/fiber';
 import { useEffect } from 'react';
-import * as THREE from 'three';
-
 
 export default function MainScene() {
 
@@ -22,22 +20,3 @@ export default function MainScene() {
         </>
     );
 };
-// export function mainScene({scene, camera, renderer, player, controllers}){
-//     const cubeGeometry = new THREE.BoxGeometry(1,1,1);
-//     const cubeMaterial = new THREE.MeshStandardMaterial({color: 'red'});
-//     const cube = new THREE.Mesh(cubeGeometry,cubeMaterial);
-//     scene.add(cube);
-//     cube.position.set(-1.5, 0.5, -1.5);
-//     cube.rotateY(Math.PI / 4);
-// }
-
-// /*
-// * Just blank for now
-// */
-// export function onFrame(
-//     delta,
-//     time,
-//     {scene, camera, renderer, player, controllers},
-// ){
-
-// };

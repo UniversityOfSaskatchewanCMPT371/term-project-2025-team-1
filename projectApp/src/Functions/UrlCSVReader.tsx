f1720a35ebabe63e098824d51581842cc10a889a
stevenromulo <poc097@usask.ca>
2025-01-28 14:02:00 -0600

    add a few more console.log() for if/else

--- projectApp/src/Functions/UrlCSVReader.tsx
+++ projectApp/src/Functions/UrlCSVReader.tsx
@@ -1,4 +1,4 @@
-import { TimeSeriesData, CSVHeaders, getCSVHeaders } from './LocalCSVReader';
+import { TimeSeriesData, CSVHeaders } from './LocalCSVReader';
 
 import Papa from 'papaparse';
 
@@ -11,6 +11,7 @@ export function UrlCSVHeaders(url:string): Promise<CSVHeaders | null> {
         if (!response.ok) {
             throw new Error(`Failed to fetch the file. Status: ${response.status}`);
         }
+        console.log('UrlCSVHeaders read successfully:',url);
         return response.text();
     }).then((csvData: string) => {
         const csvHeaders: Promise<CSVHeaders | null> = new Promise((resolve, reject) => {
@@ -19,17 +20,18 @@ export function UrlCSVHeaders(url:string): Promise<CSVHeaders | null> {
                 dynamicTyping: true,
                 complete: function(parsed: any) {
                     if(parsed.data.length == 0){
-                        console.log("EMPTY CSV FILE");
+                        console.log("UrlCSVHeaders: EMPTY CSV FILE");
                         resolve(null);
                         return;
                     }
                     const headers = Object.keys((parsed.data as string[])[0]);
                     headers.forEach((head: string) => {
                         if(head === "Time" || head === "time"){
+                            console.log("UrlCSVHeaders: Time found");
                             resolve({headers});
                         }
                     });
-                    console.log("CSV file does not contain Time");
+                    console.log("UrlCSVHeaders: CSV file does not contain Time");
                     resolve(null);
                 },
                 error: function(parseError: Error){
@@ -39,7 +41,7 @@ export function UrlCSVHeaders(url:string): Promise<CSVHeaders | null> {
         })
         return csvHeaders;
     }).catch((err) => {
-        console.log("Error:",err);
+        console.error("UrlCSVHeaders Error:",err);
         return null;
     })
 }
@@ -53,6 +55,7 @@ export function UrlCSVReader(url:string): Promise<TimeSeriesData[] | null>{
         if (!response.ok) {
             throw new Error(`Failed to fetch the file. Status: ${response.status}`);
         }
+        console.log('UrlCSVReader read successfully:',url);
         return response.text();
     }).then((csvData: string) => {
         const timeSeries: Promise<TimeSeriesData[] | null> = new Promise((resolve, reject) => {
@@ -62,28 +65,29 @@ export function UrlCSVReader(url:string): Promise<TimeSeriesData[] | null>{
                 complete: function(parsed: any) {
                     UrlCSVHeaders(url).then((csvHeaders: CSVHeaders | null) => {
                         if(csvHeaders === null){
+                            console.log('UrlCSVReader received null');
                             resolve(null);
                             return;
                         }
                         console.log("Headers: ", csvHeaders.headers);
                         
                         //Using headers as keys to assign values
-                        const typedData: TimeSeriesData[] = parsed.data.map((row:any) => (
+                        const typedData: TimeSeriesData[] = parsed.data.map((row:any) => 
                             Object.fromEntries(
                                 csvHeaders.headers.map((header: string) => [header,row[header]])
                             )
-                            
-                        ))
+                        )
                         //Just for seeing if the proper values are stored
-                        console.log("---------------")
+                        console.log("-----DATA-----")
                         console.log(typedData);
-                        console.log("---------------")
-                        console.log("Length Of", typedData.length);
+                        console.log("--------------")
+                        console.log("Length: ", typedData.length);
                         for(var i = 0; i < typedData.length; i++){
                             csvHeaders.headers.forEach((head:string) => {
                                 console.log(typedData[i]?.[head]);
                             })
                         }
+                        console.log("--------------")
                         resolve(typedData);
                     })
                 },
@@ -94,7 +98,7 @@ export function UrlCSVReader(url:string): Promise<TimeSeriesData[] | null>{
         })
         return timeSeries;
     }).catch((err) => {
-        console.log("Error:",err);
+        console.error("UrlCSVReader Error:",err);
         return null;
     })
 }

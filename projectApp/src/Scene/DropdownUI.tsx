85c10d1669e4d6b9cfea52b861b629b73ecd0494
Matthew S <mds569@usask.ca>
2025-01-29 23:07:21 -0600

    Added buttons and text to CSVUI

--- projectApp/src/Scene/DropdownUI.tsx
+++ projectApp/src/Scene/DropdownUI.tsx
@@ -1,10 +1,49 @@
+import { Text } from "@react-three/drei";
+import { events } from "@react-three/fiber";
+import { useState } from "react";
+
 export default function DropdownUI(){
+
+    /*function ButtonDD(){
+        const [hovered,hover]=useState(false);
+
+        return(
+            <mesh onPointerOver={(e) => hover(true)}
+            onPointerOut={(e) => hover(false)}>
+            </mesh><planeGeometry attach="geometry" args={[0.4,0.1]}/>
+            <meshStandardMaterial attach="material" color = "red"/>
+            />
+        )
+    }*/
 
     return(
         <>
-        <mesh>
-            <planeGeometry attach="geometry" args={[1,1]}/>
+        <mesh position={[-2, 0, 0]}>
+            <planeGeometry attach="geometry" args={[4,2]}/>
             <meshStandardMaterial attach="material" color = "blue"/>
+        </mesh>
+        <mesh rotation={[0,0,0]} position={[-2,0.6,0.01]}>
+            <Text fontSize={0.1}>
+                Add a CSV File
+            </Text>
+        </mesh>
+        <mesh rotation={[0,0,0]} position={[-2,0.1,0.01]}>
+            <Text fontSize={0.1}>
+                Enter a CSV File URL
+            </Text>
+        </mesh>
+        <mesh rotation={[0,0,0]} position={[-2,-0.1,0.01]}>
+            <planeGeometry attach="geometry" args={[1,0.1]}/>
+            <meshStandardMaterial attach="material" color = "white"/>
+        </mesh>
+        <mesh rotation={[0,0,0]} position={[-2,-0.35,0.01]}>
+            <Text fontSize={0.1}>
+                Browse for local CSV File
+            </Text>
+        </mesh>
+        <mesh rotation={[0,0,0]} position={[-2,-0.6,0.01]}>
+            <planeGeometry attach="geometry" args={[0.4,0.1]}/>
+            <meshStandardMaterial attach="material" color = "red"/>
         </mesh>
         </>
     )

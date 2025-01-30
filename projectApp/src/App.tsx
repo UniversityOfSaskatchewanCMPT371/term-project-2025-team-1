f0732dd6af44589b627257b6f8c8ef9d486b15b0
RandallSungcang <randallsungcang3@gmail.com>
2025-01-30 07:57:38 -0600

    Added extra functionality to dropdown

--- projectApp/src/App.tsx
+++ projectApp/src/App.tsx
@@ -3,6 +3,7 @@ import { Canvas } from '@react-three/fiber';
 import InitScene from './Scene/InitScene';
 import { Sky } from '@react-three/drei';
 import { Controllers, VRButton, XR } from '@react-three/xr';
+import { color } from 'three/tsl';
 
 function App() {
   
@@ -13,7 +14,7 @@ function App() {
       </div>
       <VRButton/>
       <div className="canvas-container">
-        <Canvas>
+        <Canvas color='black'>
           <XR>
             <Sky sunPosition={[0.5,0,0.5]}/>
             <ambientLight/>
--- projectApp/src/Scene/DropdownUI.tsx
+++ projectApp/src/Scene/DropdownUI.tsx
@@ -10,18 +10,28 @@ interface DropDownProps {
 interface DropDownButtonProps {
     buttonPosition: [number, number, number];
     label: string
+    type: string
 }
+
+interface AlertProps {
+    buttonPosition: [number, number, number];
+    type: string
+    
+}
+
 export default function DropDownUI({x, y, z}: DropDownProps){
     const posX = x;
     const posY = y;
     const posZ = z;
+    const [ alertActive, alert ] = useState('');
 
-    function DropDownButton({buttonPosition, label}: DropDownButtonProps){
-        const [hovered,hover]=useState(false);
+    function DropDownButton({buttonPosition, label, type}: DropDownButtonProps){
+        const [hovered,hover] = useState(false);
 
         return(
             <mesh 
                 position={buttonPosition}
+                onClick={(e) => alert(type)}
                 onPointerOver={(e) => hover(true)}
                 onPointerOut={(e) => hover(false)}>
 
@@ -34,8 +44,39 @@ export default function DropDownUI({x, y, z}: DropDownProps){
                     color={"black"}>
                     {label}
                 </Text>
+                <SelectionAlert 
+                buttonPosition={buttonPosition}
+                type={type}
+                ></SelectionAlert>
             </mesh>
-            
+        )
+    }
+
+    function SelectionAlert({buttonPosition, type}: AlertProps){
+        
+        return(
+            <>
+            <mesh 
+            position={[posX -buttonPosition[0] + 2, posY - buttonPosition[1], posZ]}
+            visible={type == alertActive}
+            >
+                <planeGeometry args={[0.8,0.5]}/>
+                <meshStandardMaterial attach="material" color ={"burlywood"}/>
+                <Text 
+                    position={[posX + 2, posY -2, posZ + 0.01]}
+                    fontSize={0.06}
+                    color={"black"}>
+                        {type} Reader Clicked !
+                </Text>
+            </mesh>
+            <mesh 
+            position={[posX + 4.5, posY, posZ]}
+            rotation={[0, 3.14,0]}
+            visible={type == alertActive}>
+                <planeGeometry args={[1,1]}/>
+                <meshStandardMaterial attach="material" color ={"burlywood"}/>
+            </mesh>
+            </>
         )
     }
 
@@ -51,21 +92,24 @@ export default function DropDownUI({x, y, z}: DropDownProps){
 
             <mesh position={[posX, posY, posZ]}>
                 <planeGeometry attach="geometry" args={[2.8,1.7]}/>
-                <meshStandardMaterial attach="material" color = "blue"/>
+                <meshStandardMaterial attach="material" color = "burlywood"/>
             </mesh>
             <mesh rotation={[0, 3.14,0]} position={[posX, posY, posZ]}>
                 <planeGeometry attach="geometry" args={[2.8,1.7]}/>
-                <meshStandardMaterial attach="material" color = "blue"/>
+                <meshStandardMaterial attach="material" color = "burlywood"/>
             </mesh>
             <mesh rotation={[0,0,0]} position={[posX,posY + 0.5, posZ + 0.01]}>
-                <Text fontSize={0.2}>
+                <Text 
+                fontSize={0.2}
+                color={"black"}>
                     Add a CSV File
                 </Text>
             </mesh>
 
             {/* For Entering File By URL */}
             <mesh rotation={[0,0,0]} position={[posX - 0.8 ,posY + 0.1,posZ + 0.01]}>
-                <Text fontSize={0.1}>
+                <Text fontSize={0.1}
+                color={"black"}>
                     Enter By URL:
                 </Text>
             </mesh>
@@ -75,17 +119,20 @@ export default function DropDownUI({x, y, z}: DropDownProps){
             </mesh>
             <DropDownButton
                 buttonPosition={[posX + 0.9, posY + 0.1, posZ + 0.01]}
-                label={"Enter"}/>
+                label={"Enter"}
+                type={"URL"}/>
 
             {/* For Loading Local File */}
             <mesh rotation={[0,0,0]} position={[posX- 0.9,posY - 0.35,posZ + 0.01]}>
-                <Text fontSize={0.1}>
+                <Text fontSize={0.1}
+                color={"black"}>
                     Load Local File: 
                 </Text>
             </mesh>
             <DropDownButton 
                 buttonPosition={[posX - 0.25,posY - 0.35,posZ + 0.01]}
-                label={'Load File'}/>
+                label={'Load File'}
+                type={"Local"}/>
         </mesh>
         </>
     )

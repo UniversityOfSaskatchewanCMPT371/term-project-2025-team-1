da329c7e645926fcaba5518ead2056df6a73c8c0
RandallSungcang <randallsungcang3@gmail.com>
2025-01-30 17:50:25 -0600

    cleaned up InitScene.tsx

--- projectApp/src/Scene/InitScene.tsx
+++ projectApp/src/Scene/InitScene.tsx
@@ -1,46 +1,17 @@
-import * as THREE from 'three';
-import { useEffect, useState } from 'react';
 import { OrbitControls } from '@react-three/drei';
-import { RoomEnvironment } from 'three/addons/environments/RoomEnvironment.js';
-import { useFrame, useThree } from '@react-three/fiber';
 import MainScene from './MainScene';
 
-//Starting scene
+/* Starting scene
+* Not really needed anymore since we are using modules that deals with initialization
+* However, could be useful for future testing when working with different scenes
+*/
 export default function InitScene(){
-    const { camera, scene, gl, clock } = useThree();
-    //const player = useRef<THREE.Object3DEventMap | null>(null);
-    const [controllers, setControllers] = useState<{ [ key: string]: any}>({
-        left: null,
-        right: null,
-    });
-
-    useEffect(() => {
-        const startScene = async () => {
-            const environment = new RoomEnvironment();
-            const pmremGenerator = new THREE.PMREMGenerator(gl);
-            scene.environment = pmremGenerator.fromScene(environment).texture;
-        };
-        startScene();
-        
-    }, [gl,scene]);
-
-    //This code is for updating the controls and looping animation
-    //Again might not need!
-    useFrame(() => {
-            Object.values(controllers).forEach((controller) => {
-                const delta = clock.getDelta();
-                const time = clock.getElapsedTime()
-                // if(controller?.gamepad){
-                //     controllers.gamepad.update();
-                // }
-            });
-            gl.render(scene, camera);
-    });
     
     return (
         <>
-            <OrbitControls />
-            <MainScene></MainScene>
+            <OrbitControls/>
+            {/* Over here we could specify the scene */}
+            <MainScene/>
         </>
     )
     

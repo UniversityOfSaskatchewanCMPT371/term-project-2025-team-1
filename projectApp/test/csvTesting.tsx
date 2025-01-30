c6d12c96c2122330b4eed2c183b00419c528f8e4
RandallSungcang <randallsungcang3@gmail.com>
2025-01-30 05:32:03 -0600

    Updating the test files

--- projectApp/test/csvTesting.tsx
+++ projectApp/test/csvTesting.tsx
@@ -1,11 +1,20 @@
 import { LocalCSVReader as reader } from "../src/Functions/LocalCSVReader.tsx"
 import { UrlCSVReader as urlReader } from "../src/Functions/UrlCSVReader.tsx"
 
-console.log("connected");
+/*
+* This File is for testing the CSV readers
+* First It will testing running the Local CSV Reader
+* Then It will test the URL CSV Reader
+* Both Functions should output the same since, it runs on the 
+* same file but different file paths/types
+*/
+
+console.log("Test Script For CSV reader");
 
 function csvTesting() {
     try{
         console.log("---------CSV1------------");
+        console.log("Entering the Local Path: ../csvTestFiles/test.csv");
         reader("../csvTestFiles/test.csv");
     }
     catch(error) {
@@ -15,6 +24,7 @@ function csvTesting() {
 function urlCsvTesting() {
     try{
         console.log("---------UrlCSV---------");
+        console.log("Entering URL: https://raw.githubusercontent.com/UniversityOfSaskatchewanCMPT371/term-project-2025-team-1/refs/heads/reactViteSpike-urlReader/csvTestFiles/test.csv")
         const url = "https://raw.githubusercontent.com/UniversityOfSaskatchewanCMPT371/term-project-2025-team-1/refs/heads/reactViteSpike-urlReader/csvTestFiles/test.csv";
         const readUrl = urlReader(url);
         readUrl.then((data) => console.log("data:",data))

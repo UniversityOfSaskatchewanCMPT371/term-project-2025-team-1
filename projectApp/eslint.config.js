89801e50892d9a6fce804009e47200aa95c20718
RandallSungcang <randallsungcang3@gmail.com>
2025-02-08 17:06:40 -0600

    Fixing build error

--- projectApp/eslint.config.js
+++ projectApp/eslint.config.js
@@ -1,4 +1,5 @@
 import js from '@eslint/js'
+import eslint from "@eslint/js";
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
@@ -7,8 +8,18 @@ import tseslint from 'typescript-eslint'
 export default tseslint.config(
   { ignores: ['dist'] },
   {
-    extends: [js.configs.recommended, 
-      ...tseslint.configs.recommended],
+    extends: [eslint.configs.recommended, 
+      tseslint.configs.recommendedTypeChecked,{
+        languageOptions: {
+          parserOptions: {
+            projectService: true,
+            tsconfigRootDir: import.meta.dirname,
+          }
+        }
+      },
+      tseslint.configs.strictTypeChecked, 
+      tseslint.configs.stylisticTypeChecked
+    ],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,

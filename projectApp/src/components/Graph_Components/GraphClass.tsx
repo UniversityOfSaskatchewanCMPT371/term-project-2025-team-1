de7568db1b6f3c819d3f75b0048de457e688b54c
Hong Wang <how763@usask.ca>
2025-02-08 14:24:06 -0600

    Create the GraphClass for Graphs

---
+++ projectApp/src/components/Graph_Components/GraphClass.tsx
@@ -0,0 +1,32 @@
+import { PointRef } from "../../types/PointInterface";
+
+// GraphClass is a class that represents a point in a graph
+// It temporarily implements the PointRef interface
+export class GraphClass implements PointRef{
+    // position of the graph (maybe updated in the future)
+    position: [number,number,number];
+    // Indicates the status of the graph (selected or not)
+    selected: boolean;
+    // xData is the x-axis data of the graph(maybe updated in the future)
+    xData: string;
+    // yData is the y-axis data of the graph(maybe updated in the future)
+    yData: number;
+
+    // Constructor initializes the GraphClass with the basic properties
+    constructor(position: [number, number, number], selected: boolean, xData: string, yData: number) {
+        this.position = position;
+        this.selected = selected;
+        this.xData = xData;
+        this.yData = yData;
+        console.log("GraphClass initialized:", this);
+    }
+
+    // Temporary test method to check if the GraphClass is working
+    testMethod() {
+        console.log("GraphClass test method called!");
+    }
+}
+
+// const graphInstance = new GraphClass([0, 0, 0], false, "TestXData", 100);
+// graphInstance.testMethod();
+

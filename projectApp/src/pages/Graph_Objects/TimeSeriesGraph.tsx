2a27895e1befecc3129c9d2b441670f44a5d102c
Alii279 <ali.abdelbaky@usask.ca>
2025-02-08 17:39:35 -0600

    Imported necessary react components and Point2D directory

--- projectApp/src/pages/Graph_Objects/TimeSeriesGraph.tsx
+++ projectApp/src/pages/Graph_Objects/TimeSeriesGraph.tsx
@@ -2,3 +2,7 @@
 //The Time Series Graph That Will Be Displayed on the Scene
 //Will require x, y positions and the number of rows
 //More expected
+import React, { useRef } from 'react';
+import { Point2D } from './Point2D'; // Point2D file in the same directory
+import { Canvas } from "@react-three/fiber"; // For the 3D canvas, since code will run on meta quest 3
+import { Box } from "@react-three/drei";     // For the cube (box geometry)

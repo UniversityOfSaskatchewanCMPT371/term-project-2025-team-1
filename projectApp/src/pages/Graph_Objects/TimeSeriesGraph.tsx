fa38abdd67f10071d452c649f9363b236947790f
Alii279 <ali.abdelbaky@usask.ca>
2025-02-08 22:37:57 -0600

    Updated TimeSeriesGraph.tsx so now it should be able to draw a graph with the points
    Used components in GraphClass to make a sample test. Will delete it later after I test it.

--- projectApp/src/pages/Graph_Objects/TimeSeriesGraph.tsx
+++ projectApp/src/pages/Graph_Objects/TimeSeriesGraph.tsx
@@ -4,5 +4,48 @@
 //More expected
 import React, { useRef } from 'react';
 import { Point2D } from './Point2D'; // Point2D file in the same directory
+import { GraphClass } from "./GraphClass"; // GraphClass file isn't currently in the same direction
 import { Canvas } from "@react-three/fiber"; // For the 3D canvas, since code will run on meta quest 3
 import { Box } from "@react-three/drei";     // For the cube (box geometry)
+
+
+const TimeSeriesGraph = () => {
+
+// Test array.
+    const timeSeriesData = [
+    { x: "2023-10-01", y: 1 },
+    { x: "2023-10-02", y: 3 },
+    { x: "2023-10-03", y: 2 },
+    { x: "2023-10-04", y: 4 },
+    { x: "2023-10-05", y: 5 },
+  ];
+
+  // each GraphClass represents one point in graph
+  const points = timeSeriesData.map((data, index) => {
+    return new GraphClass(
+      [index, data.y, 0], // Position: [x, y, z]
+      false,              // Selected: false by default
+      data.x,             // xData: Time value
+      data.y              // yData: Data value
+    );
+  });
+
+  return (
+    <Canvas>
+      <ambientLight intensity={0.5} />
+      <pointLight position={[10, 10, 10]} intensity={0.5} />
+
+      {/* Draw each point on the graph */}
+      {points.map((point, idx) => (
+        <Point2D key={idx} pointRef={point} />
+      ))}
+
+      {/* Test cube, I will remove it later */}
+      <Box position={[0, 0, 0]}>
+        <meshStandardMaterial color="gray" />
+      </Box>
+    </Canvas>
+  );
+
+};
+export default TimeSeriesGraph;

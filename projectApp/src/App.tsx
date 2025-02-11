280f7d9785c7cf5f6ee8b90e0d91a8e8826e4e7d
RandallSungcang <randallsungcang3@gmail.com>
2025-02-10 18:22:37 -0600

    Added UI start up

--- projectApp/src/App.tsx
+++ projectApp/src/App.tsx
@@ -3,17 +3,48 @@ import { Canvas } from '@react-three/fiber';
 import InitScene from './pages/Scene/InitScene';
 import { Sky } from '@react-three/drei';
 import { createXRStore, XR } from '@react-three/xr';
-import { MainController } from './controller/MainController';
+import { button, useControls } from 'leva';
 
-//Initializes and configures various parts integral to a VR experience
+//Initializes and configures various parts integral to a VR experienceq
 const store = createXRStore()
 
 //The main function for the program
 function App() {
+  //The GUI button for loading local files
+  function LoadComponent(){
+    const { load } = useControls({
+      'Load Load CSV' : button(() => alert("Loading Local File"))}) as {load: () => void}
+      return null;
+    }
+  
+  function URLComponent(){
+    const { csv } = useControls(
+      {csv: { label: 'CSV by URL', value: "Enter URL"}, button: button(() => alert("test"))} 
+    )
+    return null;
+  }
+  function UnmountedComponents(){
+    useControls(
+      'Loaded Graphs', {barValue: false, second:false}, {collapsed: true}
+    );
+
+    return null;
+  }
+
+  function AllComponents(){
+    return <>
+              <LoadComponent/>
+              <URLComponent/>
+              <UnmountedComponents/>
+            </>
+  }
   return (
     <>
       <div>
         CMPT 371: Team #1
+      </div>
+      <div>
+        <AllComponents></AllComponents>
       </div>
       {/* This button will allow the user to enter the VR Space */}
       <button onClick={() => {void (async () => store.enterAR())}}>Enter AR</button>

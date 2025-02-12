0c0c6f5dd68baf9122d40d097c605921a9b50750
Madison <madisonlabrecque@gmail.com>
2025-02-11 20:55:18 -0600

    fixed linter error

--- projectApp/src/logger-frontend.ts
+++ projectApp/src/logger-frontend.ts
@@ -2,5 +2,5 @@
 export function sendLog(message: string) {
     fetch('http://localhost:3030/log', {
         method: 'POST', body: `topic=${message}`, headers: {'Content-type': 'application/x-www-form-urlencoded'}})
-    .then(() => alert("log sent"));
+    .then(() => {alert("log sent")});
 }

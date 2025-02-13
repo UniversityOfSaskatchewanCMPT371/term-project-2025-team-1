cafb6b3ae923217d9ee979b77730171ef4c2f28c
Madison <madisonlabrecque@gmail.com>
2025-02-13 12:00:06 -0600

    added pino logger

--- projectApp/logger-server.js
+++ projectApp/logger-server.js
@@ -1,6 +1,18 @@
 import express from 'express';
 import cors from 'cors';
+import pino from 'pino';
 
+// logger code
+const logger = pino({
+    transport: {
+        targets: [
+            {target: 'pino-pretty'}, // outputs logs to console
+            {target: 'pino/file', options: {destination: 'mylogs.txt'}} // writes logs to a file
+        ]
+    }
+});
+
+// server code
 const app = express();
 const PORT = 3030;
 
@@ -9,7 +21,8 @@ app.use(express.json());
 
 app.post('/log', (req, res) => {
     let m = req.body.message;
-    console.log(m);
+    // console.log(m);
+    logger.info(m);
 
     res.sendStatus(200);
 });

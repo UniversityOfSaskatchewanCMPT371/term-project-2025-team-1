c74628e01583ead8ced6c9427c7487778489dda5
Madison <madisonlabrecque@gmail.com>
2025-02-13 22:36:54 -0600

    updated comments

--- projectApp/src/logger-frontend.ts
+++ projectApp/src/logger-frontend.ts
@@ -2,6 +2,8 @@
 // level should be one of the following: "info", "warn", "error", "fatal", or "test"
 // but error should use the sendError function below instead
 // i will add a check for a acceptable level, i just want to get this out so people can start using it - Madison
+// levels trace and debug also exist but they don't log to the locations chosen properly currently 
+// (i will fix this just wanted to get people using this asap)
 export function sendLog(level: string, message: string) {
     console.log(message);
     fetch('http://localhost:3030/log', {
@@ -15,6 +17,7 @@ export function sendLog(level: string, message: string) {
     // .then(() => {alert("log sent")});
 }
 
+// use for logging errors because we can see more information about the error this way
 // error - the error object that is thrown
 export function sendError(error: any, message: string) {
     fetch('http://localhost:3030/error', {

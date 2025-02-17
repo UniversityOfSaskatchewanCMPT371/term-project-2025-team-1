80bc0f0477907903d31b3421e27a9b7d00336398
Robert Helms <rjh746@mail.usask.ca>
2025-02-16 19:19:50 -0600

    fs mocking test added.

---
+++ projectApp/test/__mocks__/fs/promises.cts
@@ -0,0 +1,4 @@
+// code taken directly from https://vitest.dev/guide/mocking.html#file-system
+
+const { fs } = require('memfs')
+module.exports = fs.promises
---
+++ projectApp/test/fsCall.test.ts
@@ -0,0 +1,39 @@
+// code taken directly from
+// https://vitest.dev/guide/mocking.html#file-system and modified minimally for
+// linting
+
+import { beforeEach, expect, it, vi } from 'vitest'
+import { fs, vol as memVol, DirectoryJSON } from 'memfs'
+import { readHello } from './fsCall.ts'
+
+// tell vitest to use fs mock from __mocks__ folder
+// this can be done in a setup file if fs should always be mocked
+vi.mock('node:fs')
+vi.mock('node:fs/promises')
+
+beforeEach(() => {
+  // reset the state of in-memory fs
+  (memVol as unknown as { reset: () => void }).reset();
+})
+
+it('should return correct text', () => {
+  const path = '/hello-world.txt';
+  (fs as unknown as { writeFileSync: (path: string, data: string) => void }).writeFileSync(path, 'hello world')
+  const text = readHello(path)
+  expect(text).toBe('hello world')
+})
+
+it('can return a value multiple times', () => {
+  // you can use vol.fromJSON to define several files
+  (memVol as unknown as { fromJSON: (json: DirectoryJSON, cwd?: string) => void }).fromJSON(
+      {
+      './dir1/hw.txt': 'hello dir1',
+      './dir2/hw.txt': 'hello dir2',
+    },
+    // default cwd
+    '/tmp',
+  )
+
+  expect(readHello('/tmp/dir1/hw.txt')).toBe('hello dir1')
+  expect(readHello('/tmp/dir2/hw.txt')).toBe('hello dir2')
+})

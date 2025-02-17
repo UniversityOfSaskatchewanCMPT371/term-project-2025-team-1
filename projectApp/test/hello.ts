1421f89b2b2abf1bfd32f1b32db063e5879adea3
Robert Helms <rjh746@mail.usask.ca>
2025-02-16 18:38:55 -0600

    fs mocking test added.

--- projectApp/test/hello.ts
+++ projectApp/test/hello.ts
@@ -1,7 +1,7 @@
 // taken from the vitest site and modified to work with linter
 
-import { readFileSync } from 'node:fs'
+import { readFileSync, PathOrFileDescriptor } from 'node:fs'
 
-export function readHello(path) {
+export function readHello(path: PathOrFileDescriptor) {
   return readFileSync(path, 'utf-8')
 }

4337e8fc50d0149d5e502a3dad8068bcf402d4cc
IslandBlueMana <stevenrromulo@gmail.com>
2025-02-18 17:23:01 -0600

    a first look at csv fs mocking

--- projectApp/test/__mocks__/fs.ts
+++ projectApp/test/__mocks__/fs.ts
@@ -1,5 +1,16 @@
 // code taken directly from https://vitest.dev/guide/mocking.html#file-system
 
 import {fs} from "memfs";
+import { vi } from 'vitest';
+import * as memfs from 'memfs';
+
+const mockFs = memfs.fs;
+
+export const readFileSync = vi.fn(mockFs.readFileSync);
+export const writeFileSync = vi.fn(mockFs.writeFileSync);
+
+export default { readFileSync, writeFileSync };
+
+
 
 module.exports = fs
--- projectApp/test/fsCall.test.ts
+++ projectApp/test/fsCall.test.ts
@@ -3,38 +3,41 @@
 // linting
 
 import { beforeEach, expect, it, vi } from 'vitest'
-import { vol as memVol, DirectoryJSON } from 'memfs'
+import { vol as memVol } from 'memfs'
 import * as fs from '../test/__mocks__/fs';
 import { readHello } from './fsCall.ts'
+import { describe } from 'node:test';
 
 // tell vitest to use fs mock from __mocks__ folder
 // this can be done in a setup file if fs should always be mocked
 vi.mock('fs', () => import('../test/__mocks__/fs'));
 vi.mock('fs/promises', () => import('../test/__mocks__/fs/promises'));
 
-beforeEach(() => {
-  // reset the state of in-memory fs
-  (memVol as unknown as { reset: () => void }).reset();
-})
 
-it('should return correct text', () => {
-  const path = '/hello-world.txt';
-  (fs as unknown as { writeFileSync: (path: string, data: string) => void }).writeFileSync(path, 'hello world')
-  const text = readHello(path)
-  expect(text).toBe('hello world')
-})
+describe("testing fs mocking", () => {
+  beforeEach(() => {
+    // reset the state of in-memory fs
+    memVol.reset();
+  })
+  it('should return correct text', () => {
+    const path = '/hello-world.txt';
+    fs.writeFileSync(path, 'hello world')
+    const text = readHello(path)
+    expect(text).toBe('hello world')
+  })
 
-it('can return a value multiple times', () => {
-  // you can use vol.fromJSON to define several files
-  (memVol as unknown as { fromJSON: (json: DirectoryJSON, cwd?: string) => void }).fromJSON(
-      {
-      './dir1/hw.txt': 'hello dir1',
-      './dir2/hw.txt': 'hello dir2',
-    },
-    // default cwd
-    '/tmp',
-  )
+  it('can return a value multiple times', () => {
+    // you can use vol.fromJSON to define several files
+    memVol.fromJSON(
+        {
+        './dir1/hw.txt': 'hello dir1',
+        './dir2/hw.txt': 'hello dir2',
+      },
+      // default cwd
+      '/tmp',
+    )
 
-  expect(readHello('/tmp/dir1/hw.txt')).toBe('hello dir1')
-  expect(readHello('/tmp/dir2/hw.txt')).toBe('hello dir2')
+    expect(readHello('/tmp/dir1/hw.txt')).toBe('hello dir1')
+    expect(readHello('/tmp/dir2/hw.txt')).toBe('hello dir2')
+  })
 })

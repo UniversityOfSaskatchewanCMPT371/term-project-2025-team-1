0de8a49c93f0cbc738e307d046a397b130625128
IslandBlueMana <stevenrromulo@gmail.com>
2025-02-20 23:30:30 -0600

    added mock functions for fs/promise, so that fs promise now interacts with memfs properly

--- projectApp/test/__mocks__/fs/promises.ts
+++ projectApp/test/__mocks__/fs/promises.ts
@@ -1,5 +1,14 @@
 // code taken directly from https://vitest.dev/guide/mocking.html#file-system
 
-import {fs} from "memfs";
+import {fs as mockFs} from "memfs";
+import { IReadFileOptions, IWriteFileOptions } from "memfs/lib/node/types/options";
+import { vi } from 'vitest';
 
-module.exports = fs.promises
+export const readFile = vi.fn((path: string, options?: string | IReadFileOptions) => {
+    return mockFs.promises.readFile(path,options);
+});
+export const writeFile = vi.fn((path: string, data: string, options?: IWriteFileOptions) => {
+    mockFs.promises.writeFile(path,data,options);
+});
+
+export default { readFile, writeFile };

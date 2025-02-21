2d61e21f17931de5c6291739772b54624a3c16b5
IslandBlueMana <stevenrromulo@gmail.com>
2025-02-20 19:09:01 -0600

    deleting test mock fs files

--- projectApp/test/fsCall.ts
+++
@@ -1,7 +0,0 @@
-// taken from the vitest site and modified to work with linter
-
-import { readFileSync, PathOrFileDescriptor } from 'node:fs'
-
-export function readHello(path: PathOrFileDescriptor) {
-  return readFileSync(path, 'utf-8')
-}

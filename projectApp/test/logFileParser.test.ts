2c7cc209b3fbf2e35d4e03a0385800530972e4dd
Madison <madisonlabrecque@gmail.com>
2025-02-21 13:09:59 -0600

    created tests for the file parser

---
+++ projectApp/test/logFileParser.test.ts
@@ -0,0 +1,17 @@
+import { describe, expect, test } from "vitest";
+import { hasText } from "./logFileParser";
+
+
+describe('log file parser tests', () => {
+
+    test('file contains exact matching text', async() => {
+        const response = await hasText("Hello");
+        expect(response).toBe(true);
+    });
+
+    test("file does not contain given text", async() => {
+        const response = await hasText("error");
+        expect(response).toBe(false);
+    });
+
+});

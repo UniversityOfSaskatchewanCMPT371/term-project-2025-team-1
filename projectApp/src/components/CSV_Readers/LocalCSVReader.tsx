8436ab2a9acaca4572a02e8a64612be1450c4b92
Alii279 <ali.abdelbaky@usask.ca>
2025-02-21 17:22:06 -0600

    Documenting LocalCSVReader.tsx
    Adding necessary documentation and deleting 'logger' related comments because it's not necessary anymore.

--- projectApp/src/components/CSV_Readers/LocalCSVReader.tsx
+++ projectApp/src/components/CSV_Readers/LocalCSVReader.tsx
@@ -5,15 +5,13 @@ import {CSVHeaders, TimeSeriesData} from '../../types/CSVInterfaces';
 
 
 /**  
-* This function reads the headers of a csv file and stores it
+* Reads the headers of a CSV file and returns them.
 * @param file File path for csv file 
-* @returns: {Promise<CSVHeaders>}
+* @returns: {Promise<CSVHeaders>}, A promise that resolves to the CSV headers.
 **/
 export async function LocalCSVHeaders(file:string): Promise<CSVHeaders> {
-    //logger.info("Calling LocalCSVHeader ", file);
     return LocalCSVReader(file).then((timeSeries) => {
-        //logger.info("Successful LocalCSVHeader", Object.keys(timeSeries[0]));
-        return ({ headers: Object.keys(timeSeries[0]) } as CSVHeaders);
+        return ({ headers: Object.keys(timeSeries[0]) } as CSVHeaders); // Extract headers from the first row.
     // Rethrowing errors
     }).catch((err:unknown) => {
         //logger.error("LocalCSVHeaders Error", err);
@@ -22,47 +20,44 @@ export async function LocalCSVHeaders(file:string): Promise<CSVHeaders> {
 }
 
 /**  
-* This function reads the headers of a csv file and stores it
+* Reads a CSV file and returns an array of time series data.
 * @param file File path for csv file 
 * @returns: {Promise<CSVHeaders>}
 **/
 export async function LocalCSVReader(file:string): Promise<TimeSeriesData[]>{
-    //logger.info("Calling LocalCSVReader", file);
     if(!file.endsWith('.csv') && !file.endsWith('.txt')){
-        //test for files that are NOT .csv
-        //logger.error("LocalCSVReader File isn't .csv or .txt file", file);
+        // ensure input file is a csv
         throw new Error('File must be .csv or .txt');
     }
-    //logger.info("LocalCSVReader Reading file", file);
     return fsPromise.readFile(file, 'utf8').then((data: string) => {
         let timeSeries: TimeSeriesData[] = []
         Papa.parse(data, {
             header: true,
             dynamicTyping: true,
             complete: function(parsed: Papa.ParseResult<TimeSeriesData>){
-                //logger.info("LocalCSVReader Successfully parsed", file);
-                //test if casting works
+                // ensures casting works
                 timeSeries = parsed.data;
                 if(timeSeries.length === 0){
                     throw new Error("LocalCSVReader is empty");
                 }
-                //logger.info("LocalCSVReader Parsed value", timeSeries);
             },
             error: function(parseError: Error) {
-                //logger.error("LocalCSVReader Failed Parse", file);
                 throw parseError;
             }
         });
         return timeSeries;
     }).catch((err: unknown) => {
-        //test for possible error catching
+        // test for possible error catching
         throw (err as Error);
     });
 };
 
 
-//This one is for local reader but refactored to read a file
-
+/**  
+* Reads a CSV file from a File Object (local reader) and returns an array of time series data.
+* @param file File path for csv file 
+* @returns: {Promise<CSVHeaders>}
+**/
 export function LocalCsvReader(file: File): Promise<TimeSeriesData[]>{
     return new Promise<TimeSeriesData[]>((resolve, reject) => {
         if(!file.name.endsWith('.csv') && !file.name.endsWith('.txt')){
@@ -73,7 +68,7 @@ export function LocalCsvReader(file: File): Promise<TimeSeriesData[]>{
         const reader = new FileReader();
 
         reader.onload = (e) => {
-            const fileContent = e.target?.result as string;
+            const fileContent = e.target?.result as string; // Get file content as a string
 
             Papa.parse(fileContent, {
                 header: true,
@@ -91,7 +86,7 @@ export function LocalCsvReader(file: File): Promise<TimeSeriesData[]>{
         };
 
         reader.onerror = () => {
-            reject(new Error("Error"))
+            reject(new Error("Error")) // handles file reading errors
         }
         reader.readAsText(file);
         

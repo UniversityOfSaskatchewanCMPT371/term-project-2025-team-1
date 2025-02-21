30c6859a9acb93722b64e699d011e7793f079434
Madison <madisonlabrecque@gmail.com>
2025-02-21 12:56:38 -0600

    implemented function to check if a log file contains text

---
+++ projectApp/test/logFileParser.tsx
@@ -0,0 +1,24 @@
+import fs from "fs"
+import readline from "readline";
+
+export async function hasText(text: string): Promise<boolean> {
+    const filePath = "./test/logs.txt";
+
+    const fileStream = fs.createReadStream(filePath);
+    const lineReader = readline.createInterface({
+        input: fileStream
+    })
+
+    return new Promise((resolve, reject) => {
+        lineReader.on('line', (line) => {
+            if (line.includes(text)) {
+                resolve(true)
+            }
+        });
+
+        lineReader.on('close', () => {
+            resolve(false)
+        });
+        lineReader.on('error', () => reject());
+    });
+}

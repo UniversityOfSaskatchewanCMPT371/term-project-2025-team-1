374b16724fa3b4d56250ff709723cd087306cb17
Madison <madisonlabrecque@gmail.com>
2025-02-21 18:28:05 -0600

    modified test arguments

--- projectApp/test/logFileParser.test.ts
+++ projectApp/test/logFileParser.test.ts
@@ -6,7 +6,7 @@ describe('log file parser tests', () => {
     const filePath = "./test/logs.txt"
 
     test('file contains exact matching text', async() => {
-        const response = await hasText(filePath, "Hello");
+        const response = await hasText(filePath, "end of test file");
         expect(response).toBe(true);
     });
 
@@ -16,12 +16,12 @@ describe('log file parser tests', () => {
     });
 
     test("file contains matching text ignoring case", async() => {
-        const response = await hasText(filePath, "hello");
+        const response = await hasText(filePath, "TEST");
         expect(response).toBe(true);
     });
 
     test("file does not exist", async() => {
-        const response = await hasText("./jeakldsf.txt", "TEST");
+        const response = await hasText("./test.txt", "TEST");
         expect(response).toBeFalsy();
     });
 

2b9d48c061a39f8da13bef48b24af91097e4f7ae
Alii279 <ali.abdelbaky@usask.ca>
2025-02-22 03:16:34 -0600

    Fetched CSVDataObject.tsx from ID2Dev branch and added documentation to the added functions

--- projectApp/src/models/CSVDataObject.tsx
+++ projectApp/src/models/CSVDataObject.tsx
@@ -20,6 +20,26 @@ export class CSVDataObject implements CSVData{
     setData(data: { key: Record<string,string | number> }[]){
         this.data = data;
     }
+
+    async loadCSVData(index: number, file: (File | string), isUrl: boolean){
+        try {
+            
+            const data = isUrl ? await UrlCSVReader(file as string) : await LocalCsvReader(file as File)
+            this.setData(data);
+            this.setName("Graph" + index.toString());
+
+            if(data.length > 0){
+                const headers = Object.keys(data[0]);
+                this.csvHeaders = headers;
+                this.setYHeader("X");
+            }
+        }
+        catch {
+            //logger.error("Failed Loading");
+            return;
+        }
+    }
+    
     /**
     * Loads a CSV file from a local file input and sets the graph data.
     * @param index - The index used to generate a unique graph name.
@@ -29,15 +49,7 @@ export class CSVDataObject implements CSVData{
     * @throws Will silently fail if an error occurs while loading the file.
     */
     async loadLocalCSVFile(index: number,file: File){
-        try {
-            const data = await LocalCsvReader(file);
-            this.setData(data);
-            this.setName("Graph" + index.toString());
-        }
-        catch {
-            // Error: failed logging
-            return;
-        }
+        await this.loadCSVData(index, file, false);
     }
 
     /**
@@ -49,20 +61,7 @@ export class CSVDataObject implements CSVData{
      * @throws Will silently fail if an error occurs while loading the file.
      */
     async loadUrlCSVFile(index: number,file: string){
-        try {
-            const data = await UrlCSVReader(file)
-            this.setData(data);
-            this.setName("Graph" + index.toString());
-
-            if(data.length > 0){
-                const headers = Object.keys(data[0]);
-                this.csvHeaders = headers;
-            }
-        }
-        catch {
-            // Error: failed logging
-            return;
-        }
+        await this.loadCSVData(index, file, true);
     }
 
     // Loads a CSV file from a local file path (for testing purposes)
@@ -77,6 +76,27 @@ export class CSVDataObject implements CSVData{
             return;
         }
     }
+
+    /**
+     * Searches through each record and checks for the specified key in the headers.
+     * If a matching key is found, it returns the associated value from that record.
+     * 
+     * @param key - The key to search for in the dataset.
+     * @returns The corresponding value as a key-value pair (Record) from the dataset, or `null` if no match is found.
+     */
+    getDataByKey(key: string): Record<string, string | number> | null{
+        let result: Record<string, string | number> | null = null;
+        for(const value of this.data){
+            const val = value;
+            for(const header of Object.keys(val)){
+                if([header as keyof typeof val].toString() == key){
+                    result = val[header as keyof typeof val];
+                    return result;
+                }
+            }
+        }
+        return result;
+    }
     
     /**
     * Retrieves data corresponding to a specific time value.
@@ -86,10 +106,9 @@ export class CSVDataObject implements CSVData{
     getDataByTime(time:string): Record<string, string | number> | null{
         let result: Record<string, string | number> | null = null;
         for(const value of this.data){
-            // If a matching time value is found, return the corresponding yHeader value
             const val = value;
             for(const header of Object.keys(val)){
-                if(val[header as keyof typeof val].toString() == time){
+                if(val[header as keyof typeof val] as unknown as string == time){
                     result = val[this.yHeader as keyof typeof val];
                     return result;
                 }
@@ -102,7 +121,6 @@ export class CSVDataObject implements CSVData{
     getData(){
         return this.data;
     };
-
     getName(){
         return this.name;
     }
@@ -117,6 +135,18 @@ export class CSVDataObject implements CSVData{
     }
     getVRSelected(){
         return this.vrSelected;
+    };
+    getDisplayBoard(){
+        return this.displayBoard;
+    }
+    getTimeHeader():string{
+        for(const head of this.getCSVHeaders()){
+            if(head == "Time" || head =="time"){
+                return head;
+            }
+        }
+        //Error handling
+        throw new Error("No allowed time header in csv file");
     }
 
     // Setter methods
@@ -126,15 +156,37 @@ export class CSVDataObject implements CSVData{
     setBrowserSelected(bool: boolean){
         this.browserSelected = bool;
     }
-    setVRSelected(bool: boolean){
+    setVRSelected(bool:boolean){
         this.vrSelected = bool;
     }
-    // Sets the Y-axis header for graphing (must match one of the CSV headers)
     setYHeader(header:string){
-        for(const head of this.csvHeaders){
+        for(const head of this.getCSVHeaders()){
             if(head == header){
                 this.yHeader = header;
+                break;
             }
+        }
+    }
+
+    // If the display board is currently 0, it will increment to 1.
+    // If the display board is any other number, it will reset to 0.
+    incrementDisplayBoard(){
+        if(this.displayBoard == 0){
+            this.displayBoard++;
+        }
+        else{
+            this.displayBoard = 0;
+        }
+    }
+
+    // If the display board is currently 0, it will set it to 1.
+    // If the display board is any other number, it will decrement it by 1.
+    decrementDisplayBoard(){
+        if(this.displayBoard == 0){
+            this.displayBoard = 1;
+        }
+        else{
+            this.displayBoard--;
         }
     }
 }

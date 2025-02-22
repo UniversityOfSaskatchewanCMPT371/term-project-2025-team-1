3540efccf803c647adcdde99ba9f658bf03a5fce
Alii279 <ali.abdelbaky@usask.ca>
2025-02-22 02:32:36 -0600

    Documenting TimeSeriesGraph.tsx

--- projectApp/src/pages/Graph_Objects/TimeSeriesGraph.tsx
+++ projectApp/src/pages/Graph_Objects/TimeSeriesGraph.tsx
@@ -1,157 +1,181 @@
-//TODO
-//The Time Series Graph That Will Be Displayed on the Scene
-//Will require x, y positions and the number of rows
-//More expected
 import { Root, Container, Text } from '@react-three/uikit';
 import { Line } from "@react-three/drei";
 import { Create2DPoint } from '../../components/Graph_Components/Create2DPoint';
-
+import { TimeSeriesGraphClass } from '../../components/Graph_Components/TimeSeriesGraphClass';
+import { PointClass } from '../../components/Graph_Components/PointClass';
+import { useState } from 'react';
+import mainController from '../../controller/MainController';
 
 /**
  * This class will handle creating and updating a 2D Time Series graph based on the GraphClass.
  */
-// export class TimeSeriesGraph {
-//   private scene: THREE.Scene;
-//   private graph: GraphClass;
-//   private pointsMesh: THREE.Group;
 
-//   /**
-//    * Constructs a new TimeSeriesGraph instance.
-//    * @param graph - The GraphClass instance (containts the points and other graph data)
-//    * @param scene - The THREE.Scene where the graph will be displayed.
-//    */
-//   constructor(graph: GraphClass, scene: THREE.Scene) {
-//     this.graph = graph;
-//     this.scene = scene;
-//     this.pointsMesh = new THREE.Group(); // holds all point meshes
-//     this.scene.add(this.pointsMesh);
+export function TimeSeriesGraph({graph}:{graph: TimeSeriesGraphClass}){
+  const [ header, setHeader ] = useState("");
+  const [ isChanged, change ] = useState(false);
 
-//     // Initialize the graph
-//     this.addGraphBackground();
-//     this.updateGraph();
-//   }
+  const graphClass = graph;
+  const totalSpace = 5;
+  const divider = (totalSpace/graphClass.getPoints().length);
+  let current = (-1.8) + (divider/2);
+  let currentLine:[number,number,number]= ([0,0,0.01]);
+  let lastLine:[number,number,number] = ([-1.8, -1, 0.01])
+  const separator = 100/graphClass.getPoints().length;
 
-//     /**
-//    * Square object for time series graph to represent the graph
-//    */
-//   private addGraphBackground(): void {
-//     const boxGeometry = new THREE.BoxGeometry(10, 10, 0.1); // A square grid box (flattened on the Z-axis)
-//     const boxMaterial = new THREE.MeshBasicMaterial({ color: 0x00ff00, wireframe: true });
-//     const box = new THREE.Mesh(boxGeometry, boxMaterial);
-//     this.scene.add(box); // Adds the box to the scene as the background for the graph
-//   }
+  let yRange = 0; // tracks the y-axis range in the graph and increments by 5
+  const ySpacing = 100/(graphClass.timeSeriesYRange().length + 1);
 
-//   /**
-//    * Updates the graph visualization based on the current state of the GraphClass.
-//    * @param rows - The number of rows
-//    */
-//   public updateGraph(rows?: number): void {
-//     this.pointsMesh.clear();
+  function UpdateGraph(){
+    graphClass.updatePointPosition();
+    setHeader(graphClass.getYHeader());
+    mainController.updateMainScene();
 
-//     // Get the points from the GraphClass
-//     const points = this.graph.getPoints();
+    change(!isChanged)
+  }
 
-//     // Calculate spacing to make it more organized?
-//     // const rowSpacing = rows ? 1 / rows : 0;
-
-//     // Create a "mesh" for each point
-//     // Add Point2D components to the scene
-//     points.forEach(point => {
-//       const pointGeometry = new THREE.CircleGeometry(0.1, 32); // Smaller points
-//       const pointMesh = new THREE.Mesh(pointGeometry);
-//       pointMesh.position.set(point.position[0], point.position[1], point.position[2] || 0);
-//       this.pointsMesh.add(pointMesh);
-//     });
-
-//       // Add the sphere to the points group
-//       this.pointsMesh.add(pointRef);
-//     });
-//   }
-
-//   /**
-//    * Clears the graph visualization.
-//    */
-//   public clearGraph(): void {
-//     this.pointsMesh.clear();
-//   }
-// }
-
-export function TimeSeriesGraph(){
-  // function HeaderSelection(){
-  //   return(
-  //     <>
-  //     </>
-  //   )
-
-  // }
-  function GenerateSideBar(){
+  // Displays the current Y-axis header
+  function HeaderSelection(){
+    setHeader(graphClass.getYHeader());
     return(
       <>
-      <Container width={"15%"} height={"100%"} backgroundColor={"skyblue"}
-      flexDirection={"column"}>
-        <Container height={"50%"} alignItems={"flex-start"} justifyContent={"center"}>
-          <Text positionTop={10}>
-            Y Value
-          </Text>
+      <Container height={"30%"}>
+      <Text>{header}</Text>
+      </Container>
+      <Container height={"50%"} width={"100%"} justifyContent={"space-evenly"}>
+
+        {/* REFACTOR: Duplicate */}
+        <Container width={"40%"} height={"30%"} backgroundColor={"gray"} backgroundOpacity={0.8}
+        justifyContent={"center"} hover={{backgroundOpacity: 0.95}} onClick={() => {graphClass.decrementYHeader(); UpdateGraph()}}>
+          <Text fontWeight={"bold"}>&lt;</Text>
         </Container>
-        <Container height={"50%"} alignItems={"flex-start"} justifyContent={"center"}>
-        <Text>X Value</Text>
+        <Container width={"40%"} height={"30%"} backgroundColor={"gray"} backgroundOpacity={0.8}
+        justifyContent={"center"} hover={{backgroundOpacity: 0.95}} onClick={() => {graphClass.incrementYHeader(); UpdateGraph()}}>
+          <Text fontWeight={"bold"}>&gt;</Text>
         </Container>
 
       </Container>
       </>
     )
+
   }
-  // function GeneratePoints(){
-  //   return (
-  //     <>
-  //     </>
-  //   )
-  // }
-    function GenerateGraph(){
-      return (
+
+  // Sidebar displaying Y-header selection and point value information
+  function GenerateSideBar(){
+    return(
       <>
-      <Container width={"85%"} height={"100%"} flexDirection={"row"}>
-        <Container width={"10%"} flexDirection={"row-reverse"}>
-          
-        <Container
-                width = {"4%"} 
-                height = {"85%"}
-                backgroundColor= {"black"} 
-              
-            ></Container>
-            
+        <Container width={"15%"} height={"100%"} backgroundColor={"skyblue"}
+        flexDirection={"column"}> {/* Sidebar container for Y header and point values */}
+          <Container height={"50%"} alignItems={"center"} justifyContent={"center"} flexDirection={"column"}>
+            <Text>Y Header </Text>
+            <HeaderSelection></HeaderSelection>
+          </Container>
+        <Container height={"50%"} alignItems={"flex-start"} justifyContent={"center"}>
+          <Text> Point Value:</Text>
+          {/* Point data will be displayed here if selected */}
         </Container>
+      </Container>
+      </>
+    )
+  }
+
+  // Generates a point on the graph with its X and Y positions based on the data
+  function GeneratePoints({point}:{point: PointClass}){
+    point.setXPosition((current));
+    point.setYPosition(((point.getYData()/100) * (graphClass.getYRange()/(graphClass.timeSeriesYRange().length))) - (1));
+    currentLine = lastLine;
+    lastLine = ([point.getXPosition(), point.getYPosition(), 0.01])
+    return (
+      <>
+      
+       <Create2DPoint position={point.getPosition()} selected={point.getSelected()} 
+       xData={point.getXData()} yData={point.getYData()}></Create2DPoint> {/* Generate a 2D point for each graph point */}
+      </>
+    )
+  }
+
+  // Generates a line connecting two points
+  function GenerateLines(){
+    current = current + (divider);
+    return(
+      <>
+        <Line points={[[currentLine[0],currentLine[1],currentLine[2]], [lastLine[0],lastLine[1],lastLine[2]]]}
+            color={"black"} lineWidth={2}></Line>
+      </>
+    )
+  }
+
+  // increments the yRange by 5 and returns a <Text> element displaying the current value with a hyphen.
+  function GenerateYRange(){
+    yRange = yRange + 5;
+    const curVal = yRange;
+    return(
+      <>
+      <Text positionTop={10}>{curVal.toString()} -</Text>
+      </>
+    )
+  }
+
+  // Displays the points and lines
+  function GenerateGraph(){
+    return (
+      <>
+        <Container width={"85%"} height={"100%"} flexDirection={"row"}> {/* Container for graph and sidebar */}
+          <Container width={"10%"} flexDirection={"row-reverse"}>
+            <Container
+              width = {"4%"} 
+              height = {"85%"}
+              backgroundColor= {"black"} ></Container>
+
+            <Container width={"100%"} height={"85%"} flexDirection={"column-reverse"}>
+              <Container height={`${ySpacing}%`} alignContent={"baseline"} flexDirection={"row-reverse"}> 
+                <Text  positionTop={10}>0 -</Text></Container>
+              {graphClass.timeSeriesYRange().map(() => {
+                return (
+                  <>
+                  <Container width={"100%"} height={`${ySpacing}%`} alignContent={"baseline"} flexDirection={"row-reverse"}>
+                    <GenerateYRange></GenerateYRange>
+                  </Container>
+                  </>
+                )
+              })}
+
+            </Container>
+            
+            
+          </Container>
 
         <Container width={"90%"} flexDirection={"column"}>
           <Container height={"85%"} alignItems={"center"} justifyContent={"center"}>
-            
-            {/* Generate containers based on number of points, and get thoes containers position values
-                Pass these values to the point and to the line, maybe store values in a useState first
-                Then generate the objects after the contianer */}
           </Container>
-
-          <Container height={"15%"}>
-          <Container
-                width = {"100%"} 
-                height = {"4%"}
-                backgroundColor= {"black"} 
-              >
-
-              </Container>
+          
+          <Container height={"15%"} flexDirection={"column"}>
+            <Container
+              width = {"100%"} 
+              height = {"4%"}
+              backgroundColor= {"black"} >
             </Container>
+
+            <Container height={"96%"} width={"100%"}>
+            {graphClass.timeSeriesXRange().map((data) => {
+              return(
+                <Container height={"100%"} width={`${separator}%`} justifyContent={"center"}>
+                  <Text>{data}</Text>
+
+            </Container>
+              )
+            })}
+            </Container>
+          </Container>
         </Container>
 
       </Container>
-
-      
       </>
       )
     
   }
   return (
     <>
-    <mesh position={[4, 2,-3.5]}>
+    <mesh position={[4, 2,-3.5]} >
       <Root sizeX={7} sizeY={3} backgroundColor={"lightgrey"}>
         <Container 
           flexDirection={'row'}
@@ -161,16 +185,17 @@ export function TimeSeriesGraph(){
             <GenerateSideBar/>
             <GenerateGraph/>
         </Container>
-        
       </Root>
 
-      <Line
-          points={[[-1, 1, 0], [1, 0, 0]]}  // Line between two points
-          color="black"
-          lineWidth={2}
-        />
-        <Create2DPoint position={[-1, 1, 0.01]} selected={false} xData={'Time'} yData={89}/>
-        <Create2DPoint position={[1, 0, 0.01]} selected={false} xData={'Time'} yData={89}/>
+        {/* displays points and connecting lines */}
+        {graphClass.getPoints().map((points) => {
+          return(
+            <>
+            <GeneratePoints point={points}></GeneratePoints>
+            <GenerateLines/>
+            </>
+          )
+        })}
     </mesh>
     
     </>

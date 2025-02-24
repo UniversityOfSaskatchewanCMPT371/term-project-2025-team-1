48c2585f17f14d82e485ae423a4aa66d3b0f277d
Matthew S <mds569@usask.ca>
2025-02-23 21:51:02 -0600

    Update CreateTimeSeries
    Added log call declaring the creation of a time series graph object. Should this include an assertion/try-catch?

--- projectApp/src/components/Graph_Components/CreateTimeSeries.tsx
+++ projectApp/src/components/Graph_Components/CreateTimeSeries.tsx
@@ -1,8 +1,10 @@
 import { TimeSeriesGraph } from "../../pages/Graph_Objects/TimeSeriesGraph";
 import { TimeSeriesGraphClass } from "./TimeSeriesGraphClass";
+import { sendLog } from "../../logger-frontend";
 
 export function CreateTimeSeries({graphObject}:{graphObject: TimeSeriesGraphClass}){
     //const graph = new GraphClass2();
+    sendLog("info", "an TimeSeriesGraph has been created");
     return(
         <>
         <TimeSeriesGraph graph={graphObject}></TimeSeriesGraph>

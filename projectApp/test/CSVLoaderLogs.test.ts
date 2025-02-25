72bc74a7fc98186b6087993dcd0a6c1bcf57f501
IslandBlueMana <stevenrromulo@gmail.com>
2025-02-25 11:54:30 -0600

    comment out button test to be added later

--- projectApp/test/CSVLoaderLogs.test.ts
+++ projectApp/test/CSVLoaderLogs.test.ts
@@ -121,10 +121,11 @@ describe('emulating csvLoader.feature', () => {
         await expect(fileContainsText(path,LoadComponentLog)).resolves.toBe(true);
         //let me know if some other logs are left out
     });
-
-    test('emulating scenario: Deleting a loaded CSV file', () => {
-        //nothing? for now, since button has yet to have delete code implemented
-        expect("There is no delete button yet").toBe("There is no delete button yet");
+    /*
+    test('emulating scenario: Deleting a loaded CSV file', async () => {
+        const deleteButtonLog = "log for delete button"
+        await expect(fileContainsText(path,deleteButtonLog)).resolves.toBe(true);
     });
+    */
 
 });

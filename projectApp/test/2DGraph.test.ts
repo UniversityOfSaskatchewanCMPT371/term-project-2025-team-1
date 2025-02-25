2ffdc643c0355fc7d9048012e390372c0ea24c89
Matthew S <mds569@usask.ca>
2025-02-24 18:52:35 -0600

    Changed log location + uploaded example log for build purposes

--- projectApp/test/2DGraph.test.ts
+++ projectApp/test/2DGraph.test.ts
@@ -6,7 +6,8 @@ import { fileContainsText } from "./fileParser";
 
 describe('2D graph log tests', () => {
     // The log in question is located here
-    const filePath = "../logs.txt"
+    // This log is used to avoid build issues
+    const filePath = "../logs2dg.txt"
 
     // Check if the addPoint() log calls are being reached
     test('file contains addPoint() logging', async() => {

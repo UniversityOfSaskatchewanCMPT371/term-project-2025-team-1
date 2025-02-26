17b92c7d87e7f21d91ef0ba46350faad61442622
Madison <madisonlabrecque@gmail.com>
2025-02-25 22:17:13 -0600

    updated test log file in test directory

--- projectApp/test/fileParser.test.ts
+++ projectApp/test/fileParser.test.ts
@@ -3,7 +3,7 @@ import { fileContainsText } from "./fileParser";
 
 
 describe('log file parser tests', () => {
-    const filePath = "./test/logs.txt"
+    const filePath = "./test/testlogfile.txt"
 
     test('file contains exact matching text', async() => {
         const response = await fileContainsText(filePath, "end of test file");

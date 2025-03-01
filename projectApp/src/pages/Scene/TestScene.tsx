ed1db1bd62e1769d36ff876c9339e4e2b6577594
RandallSungcang <randallsungcang3@gmail.com>
2025-02-28 19:31:02 -0600

    Adding a comment

--- projectApp/src/pages/Scene/TestScene.tsx
+++ projectApp/src/pages/Scene/TestScene.tsx
@@ -6,6 +6,7 @@ export default function TestScene(): React.JSX.Element{
 
     return(
         <>
+        {/* Maybe fullscreen component stay displaying, but the container can chang visibility, so theres always that fullscreen button top left */}
             <Fullscreen flexDirection={"row"} display={clicked? "flex" : "none"}>
                 <Container flexGrow={1} backgroundOpacity={0.5} backgroundColor={"red"} onClick={()=> setClicked(!clicked)}>
 

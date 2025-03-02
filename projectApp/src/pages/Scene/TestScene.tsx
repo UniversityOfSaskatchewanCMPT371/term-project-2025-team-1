0202ba8ee684e943f43dd60c472ac0129077a896
RandallSungcang <randallsungcang3@gmail.com>
2025-03-02 10:30:07 -0600

    Setting test body space

--- projectApp/src/pages/Scene/TestScene.tsx
+++ projectApp/src/pages/Scene/TestScene.tsx
@@ -10,15 +10,22 @@ export default function TestScene(): React.JSX.Element{
         {/* Maybe fullscreen component stay displaying, but the container can chang visibility, so theres always that fullscreen button top left */}
             <Fullscreen flexDirection={"row"}>
                 {/* Main Container that encapsulates Test Scene */}
-                <Container width={"100%"} height={"100%"} backgroundColor={"purple"} backgroundOpacity={clicked? 0.5:0.01}>
-                    <Container width={"10%"} backgroundOpacity={0.5}>
+                <Container width={"100%"} height={"100%"} backgroundColor={"darkgray"} backgroundOpacity={clicked? 0.5:0.01}>
+                    <Container width={"10%"} backgroundOpacity={0.4}>
 
                     </Container>
 
                     {/* Body */}
                     <Container width={"80%"}>
-                        <Container width={"100%"} backgroundOpacity={0.5} backgroundColor={"purple"} display={clicked? "flex" : "none"}>
-
+                        <Container width={"100%"} 
+                            borderWidth={2}
+                            borderColor={"black"}
+                            justifyContent={"center"}
+                            alignContent={"center"}
+                            display={clicked? "flex":"none"}>
+                            <Text>
+                                TEST BODY
+                            </Text>
                         </Container>
                     </Container>
 

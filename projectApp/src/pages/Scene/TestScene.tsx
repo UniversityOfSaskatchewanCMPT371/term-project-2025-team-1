a41b84b74f27afcdc8a2151116243310330724e3
RandallSungcang <randallsungcang3@gmail.com>
2025-03-02 09:38:25 -0600

    Setting boundaries

--- projectApp/src/pages/Scene/TestScene.tsx
+++ projectApp/src/pages/Scene/TestScene.tsx
@@ -7,12 +7,24 @@ export default function TestScene(): React.JSX.Element{
     return(
         <>
         {/* Maybe fullscreen component stay displaying, but the container can chang visibility, so theres always that fullscreen button top left */}
-            <Fullscreen flexDirection={"row"} display={clicked? "flex" : "none"}>
-                <Container flexGrow={1} backgroundOpacity={0.5} backgroundColor={"red"} onClick={()=> setClicked(!clicked)}>
+            <Fullscreen flexDirection={"row"}>
+                {/* Main Container that encapsulates Test Scene */}
+                <Container width={"100%"} height={"100%"} backgroundColor={"purple"} backgroundOpacity={clicked? 0.5:0.01}>
+                    <Container width={"10%"} backgroundOpacity={0.5}>
 
-                </Container>
-                <Container flexGrow={1} backgroundOpacity={0.5} backgroundColor={"blue"}>
+                    </Container>
 
+                    {/* Body */}
+                    <Container width={"80%"}>
+                        <Container width={"100%"} backgroundOpacity={0.5} backgroundColor={"purple"} display={clicked? "flex" : "none"}>
+
+                        </Container>
+                    </Container>
+
+                    {/* Button to open Test Scene */}
+                    <Container width={"10%"} height={"10%"} backgroundOpacity={0.5} backgroundColor={"blue"} onClick={()=> setClicked(!clicked)}>
+
+                    </Container>
                 </Container>
             </Fullscreen>
         </>

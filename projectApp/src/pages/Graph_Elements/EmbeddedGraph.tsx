c0d3f682fdc629dfce35a3cda8d3c291a68646a9
Matthew S <mds569@usask.ca>
2025-03-04 20:55:46 -0600

    Updated EmbeddedGraph.tsx to follow basic format of TimeSeriesGraph.tsx

--- projectApp/src/pages/Graph_Elements/EmbeddedGraph.tsx
+++ projectApp/src/pages/Graph_Elements/EmbeddedGraph.tsx
@@ -4,6 +4,7 @@ import { Root, Container, Text } from "@react-three/uikit";
 import { Line, Plane } from "@react-three/drei";
 import { PointObject } from "../../components/Graph_Components/PointObject";
 import React from "react";
+import { useState } from "react";
 import mainController from "../../controller/MainController";
 import { EmbeddedGraphObject } from "../../components/Graph_Components/EmbeddedGraphObject";
 import { sendLog } from "../../logger-frontend";
@@ -16,7 +17,70 @@ import { sendLog } from "../../logger-frontend";
  */
 
 export function EmbeddedGraph({graph}:{graph: EmbeddedGraphObject}): React.JSX.Element{
+    const [ header, setHeader ] = useState("");                   //useState for changes in the graph's Y header
+    
+    // Values used to space Points in the X axis
+    //const totalSpace = 5;                                      
+    //const divider = (totalSpace/graph.getNumPoints());
+    //let current = (-1.8) + (divider/2);
+    
+    // Values used to position lines, currently set to starting position
+    let currentLine:[number,number,number]= ([0,0,0.01]);
+    let lastLine:[number,number,number] = ([-1.8, -1, 0.01])
+    
+    // Spacing used by X and Y axis
+    //const xSpacing = 100/graph.getNumPoints();
+    //const ySpacing = 100/(graph.timeSeriesYRange().length + 1);
 
+    //Used to update the graph
+    function UpdateGraph(): void{
+        //graph.updatePointPosition();
+        //setHeader(graph.getYHeader);
+        mainController.updateMainScene();
+        sendLog("info", "an EmbeddedGraph object was updated (EmbeddedGraph.tsx)");
+    }
+
+    /**
+     * Will use a PointObject to display a 3D point in the Embedded Graph
+     * @precondition PointObject, used to show Point
+     * @postcondition Interactable 3D point in the graph
+     */
+    /*function GeneratePoints({point}:{point: PointObject}): React.JSX.Element{
+        //Updating the position of the point
+        point.setXPosition((current));
+        point.setYPosition(((point.getYData()/100) * (graph.getYRange()/(graph.timeSeriesYRange().length))) - (1));
+        
+        //Updating the position of the lines based off the point position
+        currentLine = lastLine;
+        lastLine = ([point.getXPosition(), point.getYPosition(), 0.01])
+        sendLog("info", "a visual representation of points was created for a TimeSeriesGraph object (TimeSeriesGraph.tsx)");
+            
+        return (
+            <>
+            <Create3DPoint pointRef={point}></Create3DPoint>
+            </>
+        )
+    }*/
+
+    /**
+     * Updates the current position and then creates the Line which connects the last two points
+     * @postconditions The Line connecting the Points in the 3D Embedded Graph
+     */
+    /*function GenerateLines(): React.JSX.Element{
+        //current = current + (divider);
+        sendLog("info", "the lines on a EmbeddedGraph object were created (EmbeddedGraph.tsx)");
+        return(
+        <>
+            <Line points={[[currentLine[0],currentLine[1],currentLine[2]], [lastLine[0],lastLine[1],lastLine[2]]]}
+                color={"black"} lineWidth={2}></Line>
+        </>
+        )
+    }*/
+
+    /**
+     * This Function creates the main graph of the Embedded Graph
+     * @postcondition Body of the graph
+     */
     function GenerateGraph(): React.JSX.Element{
         sendLog("info", "an EmbeddedGraph visualization is being created [not yet functioning] (EmbeddedGraph.tsx)");
         return (

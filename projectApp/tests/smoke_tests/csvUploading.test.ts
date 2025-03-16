c11c8a1d7150cf894a9f338ecd3b04179572a5a8
IslandBlueMana <stevenrromulo@gmail.com>
2025-03-16 17:54:25 -0600

    changed mainController in csvUploading to use a new MainController made for these tests instead of the real mainController

--- projectApp/tests/smoke_tests/csvUploading.test.ts
+++ projectApp/tests/smoke_tests/csvUploading.test.ts
@@ -1,6 +1,6 @@
 import { describe, expect, test, vi } from "vitest";
 import "../../src/controller/MainController";
-import mainController from "../../src/controller/MainController";
+import { MainController } from "../../src/controller/MainController";
 import MockFileReader, {
   pathStrToFile,
 } from "../unit_tests/__mocks__/mockFileReader";
@@ -11,6 +11,7 @@ vi.stubGlobal("FileReader", MockFileReader);
 
 vi.stubGlobal("File", MockFile);
 
+const mainControllerTest = new MainController();
 const mainBranchUrl =
   "https://raw.githubusercontent.com/UniversityOfSaskatchewanCMPT371/term-project-2025-team-1/refs/heads/main/csvTestFiles";
 const indexedDataUrl = `${mainBranchUrl}/indexedData.csv`;
@@ -18,17 +19,21 @@ const indexedDataPath = "../csvTestFiles/indexedData.csv";
 
 describe("Test that mainController a file", () => {
   test("expect mainController to load a url", async () => {
-    await mainController.getCSVController().loadURLFile(indexedDataUrl);
+    await mainControllerTest.getCSVController().loadURLFile(indexedDataUrl);
     //should be Graph0
-    const urlGraph = mainController.getCSVController().getDataByName("Graph0");
+    const urlGraph = mainControllerTest
+      .getCSVController()
+      .getDataByName("Graph0");
     expect(urlGraph).not.toBeNull();
     expect(urlGraph).toBeInstanceOf(CSVDataObject);
   });
   test("Test that mainController can load a local csv file", async () => {
     const indexedDataFile = await pathStrToFile(indexedDataPath);
-    await mainController.getCSVController().loadLocalFile(indexedDataFile);
+    await mainControllerTest.getCSVController().loadLocalFile(indexedDataFile);
     //should be Graph1
-    const fileGraph = mainController.getCSVController().getDataByName("Graph1");
+    const fileGraph = mainControllerTest
+      .getCSVController()
+      .getDataByName("Graph1");
     expect(fileGraph).not.toBeNull();
   });
 });

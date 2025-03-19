24c0c9e760c13eb5f07351b6c490d7a1976fdc9b
ona341 <aaghaizu@yahoo.com>
2025-03-18 21:14:18 -0600

    Changed BaseInterface to take an instance and not an array of datainterface instances
    Changed BaseInterface to take an instance and not an array of datainterface instances

--- projectApp/src/types/BaseInterfaces.tsx
+++ projectApp/src/types/BaseInterfaces.tsx
@@ -1,14 +1,14 @@
 export interface ModelInterface {
-  data: DataInterface[]; //Array of data assigned to model
+  data: DataInterface; //Array of data assigned to model
 
-  getData(): DataInterface[]; //Get the array of data   //Uses name to find the CSVData
+  getData(): DataInterface; //Get the array of data   //Uses name to find the CSVData
 }
 
 export interface ControllerInterface {
   model: ModelInterface; //Model Associated with the controller
 
   getModel(): ModelInterface; //Get the model associated with the controller
-  getModelData(): DataInterface[]; //Get the data array of Model
+  getModelData(): DataInterface; //Get the data array of Model
 }
 
 export interface DataInterface {

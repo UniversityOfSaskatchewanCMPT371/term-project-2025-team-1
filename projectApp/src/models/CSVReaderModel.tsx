a46b1783823286b5cace24eff98fbe6599e9140f
ona341 <aaghaizu@yahoo.com>
2025-03-20 13:38:22 -0600

    Cleaned up CSVReaderModel

--- projectApp/src/models/CSVReaderModel.tsx
+++ projectApp/src/models/CSVReaderModel.tsx
@@ -21,7 +21,6 @@ export class CSVReaderModel implements CSVModelInterface {
   data?: CSVDataObject;
 
   constructor() {
-    // this.data = [];
   }
 
   /**
@@ -34,18 +33,9 @@ export class CSVReaderModel implements CSVModelInterface {
    *
    * @returns The CSVData object if found, otherwise null.
    */
-  getCSVFileByName(name: string): CSVDataObject | undefined {
-    // for (const data of this.data) {
-    //   if (data.name == name) {
+  getCSVFile(): CSVDataObject | undefined {
     return this.data;
   }
-  //   }
-  //   sendLog(
-  //     "info",
-  //     `getCSVRileByName could not find file ${name}, is this expected behavior?`,
-  //   );
-  //   return null;
-  // }
 
   /**
    * Reads a local CSV file and sets it as the data array.

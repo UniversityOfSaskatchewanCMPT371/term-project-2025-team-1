076b763e7074e32132056285d1d5a85d9619eb24
Matthew S <mds569@usask.ca>
2025-03-22 00:01:30 -0600

    Updated beforeAll
    this is supposed to create a graph object to use for the tests but is still in progress

--- projectApp/tests/smoke_tests/graph.test.ts
+++ projectApp/tests/smoke_tests/graph.test.ts
@@ -1,8 +1,20 @@
-import { describe, expect, test, vi } from "vitest";
+import { describe, test, beforeAll } from "vitest";
 import "../../src/controller/MainController";
 import { MainController } from "../../src/controller/MainController";
+import { pathStrToFile } from "../unit_tests/__mocks__/mockFileReader";
 
+const mainControllerTest = new MainController();
+const mainBranchUrl =
+  "https://raw.githubusercontent.com/UniversityOfSaskatchewanCMPT371/term-project-2025-team-1/refs/heads/main/csvTestFiles";
+const indexedDataUrl = `${mainBranchUrl}/indexedData.csv`;
+const indexedDataPath = "../csvTestFiles/indexedData.csv";
 
 describe("Test that graph objects are properly created", () => {
-
+    beforeAll(async () => {
+        const indexedDataFile = await pathStrToFile(indexedDataPath);
+        await mainControllerTest.getCSVController().loadLocalFile(indexedDataFile);
+        mainControllerTest.getCSVController().generate();
+        mainControllerTest.getGraphController().getModel()
+    })
+    test("expect")
 });

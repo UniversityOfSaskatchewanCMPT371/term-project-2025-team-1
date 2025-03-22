b3518194b255ad089d86993dfe0a61d00dc5f6fb
Matthew S <mds569@usask.ca>
2025-03-21 22:02:33 -0600

    Create graph.test.ts

---
+++ projectApp/tests/smoke_tests/graph.test.ts
@@ -0,0 +1,8 @@
+import { describe, expect, test, vi } from "vitest";
+import "../../src/controller/MainController";
+import { MainController } from "../../src/controller/MainController";
+
+
+describe("Test that graph objects are properly created", () => {
+
+});

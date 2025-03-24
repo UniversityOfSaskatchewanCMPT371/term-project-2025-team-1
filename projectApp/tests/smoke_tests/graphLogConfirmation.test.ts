e5521fa77a1ce939cae11ae3a951114747653f6f
Matthew S <mds569@usask.ca>
2025-03-23 22:47:49 -0600

    Ran prettier

--- projectApp/tests/smoke_tests/graphLogConfirmation.test.ts
+++ projectApp/tests/smoke_tests/graphLogConfirmation.test.ts
@@ -11,85 +11,80 @@ test("log file exists", () => {
 });
 
 describe("Scenario: ", () => {
-
   test("Check that the main scene was updated with a TimeSeriesGraph", async () => {
     const response = await fileContainsText(
-        filePath,
-        `a visual representation of points was created for a TimeSeriesGraph object`,
+      filePath,
+      `a visual representation of points was created for a TimeSeriesGraph object`,
     );
     expect(response).toBe(true);
   });
 
   test("Check that setYData() was called", async () => {
     const response = await fileContainsText(
-        filePath,
-        `setYData() was called; y Data of Point set`,
+      filePath,
+      `setYData() was called; y Data of Point set`,
     );
     expect(response).toBe(true);
   });
 
   test("Check that setTimeData() was called", async () => {
     const response = await fileContainsText(
-        filePath,
-        `setTimeData() was called; time Data of Point set`,
+      filePath,
+      `setTimeData() was called; time Data of Point set`,
     );
     expect(response).toBe(true);
   });
 
   test("Check that the points were populated", async () => {
     const response = await fileContainsText(
-        filePath,
-        `populatePoints() was called`,
+      filePath,
+      `populatePoints() was called`,
     );
     expect(response).toBe(true);
   });
 
   test("Check that 3D point positions were set", async () => {
     const response = await fileContainsText(
-        filePath,
-        `setPoint3DPosition() was called`,
+      filePath,
+      `setPoint3DPosition() was called`,
     );
     expect(response).toBe(true);
   });
 
   test("Check that vector position is calculated along with point position setting", async () => {
     const response = await fileContainsText(
-        filePath,
-        `vector position calculated`,
+      filePath,
+      `vector position calculated`,
     );
     expect(response).toBe(true);
   });
 
   test("Check that the embedded graph is instantiated with points", async () => {
     const response = await fileContainsText(
-        filePath,
-        `Points added to EmbeddedGraphObject`,
+      filePath,
+      `Points added to EmbeddedGraphObject`,
     );
     expect(response).toBe(true);
   });
 
   test("Check that each graph was created from a properly read CSV object", async () => {
     const response = await fileContainsText(
-        filePath,
-        `pushDataToModel() was called`,
+      filePath,
+      `pushDataToModel() was called`,
     );
     expect(response).toBe(true);
   });
 
   test("Check that main scene is aware of the graph object update", async () => {
-    const response = await fileContainsText(
-        filePath,
-        `Update main scene`,
-    );
+    const response = await fileContainsText(filePath, `Update main scene`);
     expect(response).toBe(true);
   });
 
   test("Check that a TimeSeries Graph was generated", async () => {
     const response = await fileContainsText(
-        filePath,
-        `generateTimeSeriesGraph() was called`,
+      filePath,
+      `generateTimeSeriesGraph() was called`,
     );
     expect(response).toBe(true);
   });
-
 });

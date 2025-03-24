34a76b476e3c66ab04cf4e9e8ce61acee05f7c4a
IslandBlueMana <stevenrromulo@gmail.com>
2025-03-23 21:47:39 -0600

    updated csvLoaderLogs.test.ts with new log tests

--- projectApp/tests/logConfirmationTests/CSVLoaderLogs.test.ts
+++ projectApp/tests/logConfirmationTests/CSVLoaderLogs.test.ts
@@ -1,53 +1,71 @@
 import { describe, expect, test } from "vitest";
-//import { sendLog } from "../src/logger-frontend";
 import * as fs from "fs";
-
 import { fileContainsText } from "./fileParser";
+
+// Testing BrowserUI and its subsequent calls give the correct logs
+// Run this test after following csvLoader.feature Gherkin steps
 
 describe("emulating csvLoader.feature", () => {
   const path = "./tests/logConfirmationTests/logConfirmationlogs.txt";
   test("log file exists", () => {
-    /*
-        const testInfo = `test for log at path ${path}`;
-        const testError = new Error("test error");
-        const testErrMessage = `test for error at path ${path}`;
-        */
     expect(fs.existsSync(path)).toBe(true);
-    /*
-        sendLog("info",testInfo);
-        expect(fileContinsText(path,testInfo));
-        sendError(testError,testErrMessage);
-        expect(fileContinsText(path,testError));
-        expect(fileContinsText(path,testErrMessage));
-        */
-    //re-add these when actual gherkin testing is done
   });
+
   test("emulating scenario: Confirming URL CSV entry", async () => {
-    //UrlCsvReader
-    const URLCSVReaderSuccess = "URLCSVReader has successfully parsed";
-    //CSVDataObject
-    const CSVDataObjectSetName = "will now be called";
-    const CSVDataObjectSetYHeader = "yHeader is set to";
-    const loadCSVDataSuccess = "loadCSVData has loaded csv data";
-    //CSVReaderModel
-    const readURLFileSuccess = "readURLFile read a file";
-    //BrowserUI
-    const URLComponentLog = "URLComponent read:";
+    // UrlCsvReader
+    const URLCSVReaderSuccess = "URLCSVReader has successfully parsed"; // omitting {csv} string
     await expect(fileContainsText(path, URLCSVReaderSuccess)).resolves.toBe(
       true,
     );
+
+    const CSVDataObjectSetData =
+      "setData() was called, data has been set (CSVDataObject.ts)";
+    await expect(fileContainsText(path, CSVDataObjectSetData)).resolves.toBe(
+      true,
+    );
+
+    // CSVDataObject
+    const CSVDataObjectSetName = "setName,  will now be called Graph0";
     await expect(fileContainsText(path, CSVDataObjectSetName)).resolves.toBe(
       true,
     );
+
+    const CSVDataObjectFindTimeHeader =
+      "findTimeHeader() was called, the time header in the data set has been found (CSVDataObject.ts)";
+    await expect(
+      fileContainsText(path, CSVDataObjectFindTimeHeader),
+    ).resolves.toBe(true);
+
+    const CSVDataObjectSetTimeHeader =
+      "setTimeHeader() was called, finding the time header in the data set (CSVDataObject.ts)";
+    await expect(
+      fileContainsText(path, CSVDataObjectSetTimeHeader),
+    ).resolves.toBe(true);
+
+    const CSVDataObjectFindFirstHeader =
+      "findFirstHeader() was called, the first header was found Some (CSVDataObject.ts)";
+    await expect(
+      fileContainsText(path, CSVDataObjectFindFirstHeader),
+    ).resolves.toBe(true);
+
+    const CSVDataObjectSetYHeader = "setYHeader, Graph0 yHeader is set to Some";
     await expect(fileContainsText(path, CSVDataObjectSetYHeader)).resolves.toBe(
       true,
     );
-    await expect(fileContainsText(path, loadCSVDataSuccess)).resolves.toBe(
-      true,
-    );
+
+    const CSVDataObjectLoadSuccess = "loadCSVData has loaded csv data";
+    await expect(
+      fileContainsText(path, CSVDataObjectLoadSuccess),
+    ).resolves.toBe(true);
+    // CSVReaderModel
+
+    const readURLFileSuccess = "readURLFile read a file"; // omitting url path
     await expect(fileContainsText(path, readURLFileSuccess)).resolves.toBe(
       true,
     );
+
+    // BrowserUI
+    const URLComponentLog = "URLComponent read:"; // omitting url path
     await expect(fileContainsText(path, URLComponentLog)).resolves.toBe(true);
   });
 
@@ -55,84 +73,126 @@ describe("emulating csvLoader.feature", () => {
     //depends on what counts as invalid
     //it was not .csv, or fetch could not find url, or file was empty, or other error
     //UrlCsvReader
-    const UrlReaderNotCSV = "is not .csv";
+    const UrlReaderNotCSV = "Final URL destination is not csv readable";
     const UrlReaderIsEmpty = "URLCSVReader is empty";
-    const UrlCSVReaderError = "URLCSVReader error";
-    //CSVDataObject
-    const loadCSVDataError = "loadCSVData error";
-    //CSVReaderModel
-    const readURLFileError = "readURLFile error";
-    //BrowserUI
-    const URLComponentLog = "URLComponent read:";
+    const UrlReaderFetchFail = "Failed to fetch the file";
     const urlAnyInvalid = Promise.all([
       fileContainsText(path, UrlReaderNotCSV),
       fileContainsText(path, UrlReaderIsEmpty),
-      fileContainsText(path, UrlCSVReaderError),
+      fileContainsText(path, UrlReaderFetchFail),
     ]).then((items) => items.includes(true));
     await expect(urlAnyInvalid).resolves.toBe(true);
-    //expect that loadCSVDataError rethrows errors
+    
+    const UrlCSVReaderError = "URLCSVReader error";
+    await expect(fileContainsText(path, UrlCSVReaderError)).resolves.toBe(true);
+
+    //CSVDataObject
+    const loadCSVDataError = "loadCSVData error";
     await expect(fileContainsText(path, loadCSVDataError)).resolves.toBe(true);
-    //expect that readURLFileError rethrows errors
+
+    //CSVReaderModel
+    const readURLFileError = "readURLFile error";
     await expect(fileContainsText(path, readURLFileError)).resolves.toBe(true);
-    //when URLComponent implements try/catch error, replace with its message
+
+    // CSVController
+    const loadURLFileError = "loadURLFile Error";
+    await expect(fileContainsText(path, loadURLFileError)).resolves.toBe(true);
+
+    //BrowserUI
+    const URLComponentLog = "URLComponent read:";
     await expect(fileContainsText(path, URLComponentLog)).resolves.toBe(true);
   });
 
   test("emulating scenario: Confirming Local CSV entry", async () => {
     //LocalCsvReader
-    const LocalCSVReaderSuccess = "LocalCsvReader(file) has read data";
-    //CSVDataObject
-    const CSVDataObjectSetName = "will now be called";
-    const CSVDataObjectSetYHeader = "yHeader is set to";
-    const loadCSVDataSuccess = "loadCSVData has loaded csv data";
-    //CSVReaderModel
-    const readLocalFileSuccess = "readLocalFile read a file";
-    //BrowserUI
-    const LoadComponentLog = "LoadComponent read:";
+    const LocalCSVReaderSuccess = "LocalCsvReader(file) has read data"; // omitting {csv} string
     await expect(fileContainsText(path, LocalCSVReaderSuccess)).resolves.toBe(
       true,
     );
+
+    const CSVDataObjectSetData =
+      "setData() was called, data has been set (CSVDataObject.ts)";
+    await expect(fileContainsText(path, CSVDataObjectSetData)).resolves.toBe(
+      true,
+    );
+
+    // CSVDataObject
+    const CSVDataObjectSetName = "setName,  will now be called Graph1"; // second valid graph
     await expect(fileContainsText(path, CSVDataObjectSetName)).resolves.toBe(
       true,
     );
+
+    const CSVDataObjectFindTimeHeader =
+      "findTimeHeader() was called, the time header in the data set has been found (CSVDataObject.ts)";
+    await expect(
+      fileContainsText(path, CSVDataObjectFindTimeHeader),
+    ).resolves.toBe(true);
+
+    const CSVDataObjectSetTimeHeader =
+      "setTimeHeader() was called, finding the time header in the data set (CSVDataObject.ts)";
+    await expect(
+      fileContainsText(path, CSVDataObjectSetTimeHeader),
+    ).resolves.toBe(true);
+
+    const CSVDataObjectFindFirstHeader =
+      "findFirstHeader() was called, the first header was found Some (CSVDataObject.ts)";
+    await expect(
+      fileContainsText(path, CSVDataObjectFindFirstHeader),
+    ).resolves.toBe(true);
+
+    const CSVDataObjectSetYHeader = "setYHeader, Graph0 yHeader is set to Some";
     await expect(fileContainsText(path, CSVDataObjectSetYHeader)).resolves.toBe(
       true,
     );
-    await expect(fileContainsText(path, loadCSVDataSuccess)).resolves.toBe(
-      true,
-    );
+
+    const CSVDataObjectLoadSuccess = "loadCSVData has loaded csv data";
+    await expect(
+      fileContainsText(path, CSVDataObjectLoadSuccess),
+    ).resolves.toBe(true);
+
+    //CSVReaderModel
+    const readLocalFileSuccess = "readLocalFile read a file"; // omitting file path
     await expect(fileContainsText(path, readLocalFileSuccess)).resolves.toBe(
       true,
     );
+
+    //BrowserUI
+    const LoadComponentLog = "LoadComponent read:"; // omitting file path
     await expect(fileContainsText(path, LoadComponentLog)).resolves.toBe(true);
   });
 
   test("emulating scenario: Confirming invalid Local CSV entry", async () => {
     //depends on what counts as invalid
-    //it was not .csv, or fetch could not find url, or file was empty, or other error
-    //LocalCsvReader
-    const LocalReaderNotCSV = "This file is not csv";
-    //there is no method of checking if the file is empty with LocalCsvReader(file:File)
-    const LocalCSVReaderError = "LocalCsvReader(file) has errored for";
-    //CSVDataObject
-    const loadCSVDataError = "loadCSVData error";
-    //CSVReaderModel
-    const readLocalFileError = "readLocalFile error";
-    //BrowserUI
-    const LoadComponentLog = "LoadComponent read:";
-
-    const localAnyInvalid = Promise.all([
+    //it was not .csv, or file was empty, or other error
+    //UrlCsvReader
+    const LocalReaderNotCSV = "LocalCsvReader(file) receives a non csv file";
+    const LocalReaderIsEmpty = "Empty file set";
+    const LocalCSVReaderError = "LocalCsvReader(file) has errored";
+    const urlAnyInvalid = Promise.all([
       fileContainsText(path, LocalReaderNotCSV),
+      fileContainsText(path, LocalReaderIsEmpty),
       fileContainsText(path, LocalCSVReaderError),
     ]).then((items) => items.includes(true));
-    await expect(localAnyInvalid).resolves.toBe(true);
-    //expect that loadCSVDataError rethrows errors
+    await expect(urlAnyInvalid).resolves.toBe(true);
+    
+    //CSVDataObject
+    const loadCSVDataError = "loadCSVData error";
     await expect(fileContainsText(path, loadCSVDataError)).resolves.toBe(true);
-    //expect that readURLFileError rethrows errors
+
+    //CSVReaderModel
+    const readLocalFileError = "readLocalFile error";
     await expect(fileContainsText(path, readLocalFileError)).resolves.toBe(
       true,
     );
-    //when LoadComponent implements try/catch error, replace with its message
+
+    // CSVController
+    const loadLocalFileError = "loadLocalFile Error";
+    await expect(fileContainsText(path, loadLocalFileError)).resolves.toBe(
+      true,
+    );
+
+    //BrowserUI
+    const LoadComponentLog = "LoadComponent Return Error";
     await expect(fileContainsText(path, LoadComponentLog)).resolves.toBe(true);
   });
 });

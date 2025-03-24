47aa6e0e47b37c42806e34001d51a12151741ba1
Matthew S <mds569@usask.ca>
2025-03-23 21:18:30 -0600

    FIrst draft of graph.test.ts

--- projectApp/tests/smoke_tests/graph.test.ts
+++ projectApp/tests/smoke_tests/graph.test.ts
@@ -1,25 +1,52 @@
 import { describe, test, beforeAll, expect } from "vitest";
 import "../../src/controller/MainController";
-import { MainController } from "../../src/controller/MainController";
+import mainController from "../../src/controller/MainController";
 
-const mainControllerTest = new MainController();
 const mainBranchUrl =
-  "https://raw.githubusercontent.com/UniversityOfSaskatchewanCMPT371/term-project-2025-team-1/refs/heads/main/csvTestFiles";
+  "https://raw.githubusercontent.com/UniversityOfSaskatchewanCMPT371/term-project-2025-team-1/refs/heads/ID4/csvTestFiles";
 const indexedDataUrl = `${mainBranchUrl}/indexedData.csv`;
 
 describe("Test that graph objects are properly created", () => {
     beforeAll(async () => {
-        //const indexedDataFile = await pathStrToFile(indexedDataPath);
-        await mainControllerTest.getCSVController().loadURLFile(indexedDataUrl);
-        mainControllerTest.getCSVController().generate();
+        await mainController.getCSVController().loadURLFile(indexedDataUrl);
+        mainController.getCSVController().generate(1);
     })
-    test("expect graph model to contain a 10 point graph", async () =>{
-        const graphm = mainControllerTest.getGraphController().getModel();
-        expect(graphm.getData().at(0)?.getNumPoints()).toBe(10);
+    test("expect Time Series graph model to contain a 10 point graph", async () =>{
+        const graphm = mainController.getGraphController().getModel();
+        expect(graphm.getData()[0].getNumPoints()).toBe(10);
     });
-    test("expect correct points to be calculated", async () =>{
-        const graphm = mainControllerTest.getGraphController().getModel();
-        const points = graphm.getData()[0].getPoints();
+    test("expect Embedded graph model to contain a 10 point graph", async () =>{
+        const graphm = mainController.getGraphController().getModel();
+        expect(graphm.getEmbeddedGraphData()[0].getPoints3D().length).toBe(10);
+    });
+    test("expect correct Time Series Graph points to be calculated", async () =>{
+        const graphm = mainController.getGraphController().getModel();
+        const points = graphm.getData()[0].get2DPoints();
+        console.log(points);
+        expect(points[0].getObject().getTimeData()).toBe(0);
+        expect(points[0].getObject().getYData()).toBe(1);
+        expect(points[1].getObject().getTimeData()).toBe(1);
+        expect(points[1].getObject().getYData()).toBe(2);
+        expect(points[2].getObject().getTimeData()).toBe(2);
+        expect(points[2].getObject().getYData()).toBe(3);
+        expect(points[3].getObject().getTimeData()).toBe(3);
+        expect(points[3].getObject().getYData()).toBe(4);
+        expect(points[4].getObject().getTimeData()).toBe(4);
+        expect(points[4].getObject().getYData()).toBe(5);
+        expect(points[5].getObject().getTimeData()).toBe(5);
+        expect(points[5].getObject().getYData()).toBe(6);
+        expect(points[6].getObject().getTimeData()).toBe(6);
+        expect(points[6].getObject().getYData()).toBe(7);
+        expect(points[7].getObject().getTimeData()).toBe(7);
+        expect(points[7].getObject().getYData()).toBe(8);
+        expect(points[8].getObject().getTimeData()).toBe(8);
+        expect(points[8].getObject().getYData()).toBe(9);
+        expect(points[9].getObject().getTimeData()).toBe(9);
+        expect(points[9].getObject().getYData()).toBe(10);
+    });
+    test("expect correct Embedded Graph points to be calculated", async () =>{
+        const graphm = mainController.getGraphController().getModel();
+        const points = graphm.getEmbeddedGraphData()[0].getPoints3D();
         expect(points[0].getPosition()).toStrictEqual([1, 0, 0]);
         expect(points[1].getPosition()).toStrictEqual([2, 1, 0]);
         expect(points[2].getPosition()).toStrictEqual([3, 2, 1]);
@@ -31,11 +58,11 @@ describe("Test that graph objects are properly created", () => {
         expect(points[8].getPosition()).toStrictEqual([9, 8, 7]);
         expect(points[9].getPosition()).toStrictEqual([10, 9, 8]);
     });
-    test("expect correct range for both axes", async () =>{
-        const graphm = mainControllerTest.getGraphController().getModel();
-        const graphaxes = graphm.getData()[0].getAxes();
-        expect(graphaxes.xRange[1]).toBe(10);
-        expect(graphaxes.yRange[1]).toBe(10);
+    test("expect correct Time Series range", async () =>{
+        const graphm = mainController.getGraphController().getModel();
+        const graphaxes = graphm.getData()[0].getYRange();
+        expect(graphaxes).toBe(0);
+        // Currently expect y range to be zero, waiting on future implementation
     });
 
 });

60feab5203fd920e0caadab8fbfd5e1eeea7000f
IslandBlueMana <stevenrromulo@gmail.com>
2025-03-31 16:46:06 -0600

    updated comments to be recognized as docstrings

--- projectApp/src/logger-frontend.ts
+++ projectApp/src/logger-frontend.ts
@@ -1,12 +1,13 @@
 // Note: trace and debug levels also exist but they don't log to the locations chosen currently
-
-/* sendLog: Creates and sends a log to the log server
-    @precondition: 
-        Level should be one of the following: "info", "warn", "error", "fatal", or "test", which determines severity of log message
-        message != null
-    @postcondition:
-        A log will be written to the log server with the set level and message, along with timestamp etc.
-*/
+/**
+ * sendLog: Creates and sends a log to the log server
+ * @param level level of the log 
+ * @param message non-empty message string describing the log
+ * @precondition
+ * - Level should be one of the following: "info", "warn", "error", "fatal", or "test", which determines severity of log message
+ * - message != null
+ * @postcondition A log will be written to the log server with the set level and message, along with timestamp etc.
+ */
 export function sendLog(level: string, message: string) {
   // Send the log to the log server
   fetch("http://localhost:3030/log", {
@@ -20,13 +21,18 @@ export function sendLog(level: string, message: string) {
   // .then(() => {alert("log sent")});
 }
 
-/* sendError: Creates and sends an *error* log to the log server (used specifically in error cases to provide more info)
-    @precondition:
-        "error" must represent the error object that is thrown 
-        message != null
-    @postcondition:
-        An error log will be written to the log server with additional messaging to provide context regarding the error
-*/
+    
+
+/**
+ * sendError: Creates and sends an *error* log to the log server
+ * (used specifically in error cases to provide more info)
+ * @param error type of error that is sent
+ * @param message non-empty message string describing the error context
+ * @precondition
+ * - "error" must represent the error object that is thrown 
+ * - message != null
+ * @postcondition An error log will be written to the log server with additional messaging to provide context regarding the error
+ */
 export function sendError(error: any, message: string) {
   // Send an error-specific log to the log server
   fetch("http://localhost:3030/error", {

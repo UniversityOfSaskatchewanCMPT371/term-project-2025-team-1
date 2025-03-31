eca6d60c45fa3b3ebe4a4a22138b65728e9e1da8
Madison <madisonlabrecque@gmail.com>
2025-03-31 14:12:48 -0600

    Revert "removed list of graphs in browserUI"
    This reverts commit 87059e16003084564f987de2355b87f71e581f9b.

--- projectApp/src/pages/UI/BrowserUI.tsx
+++ projectApp/src/pages/UI/BrowserUI.tsx
@@ -120,11 +120,37 @@ export default function BrowserUI() {
     );
   }
 
+  /**
+   * Component that displays the loaded csv files on the browser UI
+   * @preconditions None
+   * @postconditions Unmounted component that displays loaded csv files when opened
+   */
+  function UnmountedComponents(): null {
+    const names: [string, boolean][] = mainController
+      .getCSVController()
+      .browserCSVFiles();
+
+    //Setting the objects to be displayed
+    const controlsObject: Record<string, boolean | ButtonInput> = names.reduce(
+      (acc, [name, value]) => {
+        acc[name] = value;
+
+        sendLog("info", `UnmountedComponents unmount: ${String(controlKey)}`);
+        return acc;
+      },
+      {} as Record<string, boolean | ButtonInput>,
+    );
+
+    useControls(`Loaded Graphs`, controlsObject, { collapsed: true });
+
+    return null;
+  }
 
   return (
     <>
       <URLComponent />
       <LoadComponent />
+      <UnmountedComponents />
     </>
   );
 }

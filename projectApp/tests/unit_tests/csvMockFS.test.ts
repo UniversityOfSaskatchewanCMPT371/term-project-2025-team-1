a0ed883c39feb9209bdf880e9237d385c836629b
Madison <madisonlabrecque@gmail.com>
2025-03-31 13:23:54 -0600

    fixed file system mocking tests to not use the readers

--- projectApp/tests/unit_tests/csvMockFS.test.ts
+++ projectApp/tests/unit_tests/csvMockFS.test.ts
@@ -2,12 +2,10 @@
 // https://vitest.dev/guide/mocking.html#file-system
 // modified to agree with eslint
 
-import { beforeEach, expect, test, vi } from "vitest";
-import { describe } from "node:test";
+import { beforeEach, describe, expect, test, vi } from "vitest";
 import { vol as memVol } from "memfs";
 import * as fs from "./__mocks__/fs";
 import * as fsPromise from "./__mocks__/fs/promises";
-import { LocalCSVReader as localReader } from "../../src/components/Csv_Components/CSVReaders";
 
 // tell vitest to use fs mock from __mocks__ folder
 // this can be done in a setup file if fs should always be mocked
@@ -15,6 +13,13 @@ vi.mock("fs", () => import("./__mocks__/fs"));
 vi.mock("fs/promises", () => import("./__mocks__/fs/promises"));
 
 describe("testing fs mocking", () => {
+  const path = "/valid.csv";
+  const data = `Time,X,Y
+                "2025-01-18",10,15
+                "2025-01-19",20,25
+                "2025-01-20",30,35
+                "2025-01-21",40,45`;
+
   //using a mock fs, memfs, to virtually test localReader
   beforeEach(() => {
     // reset the state of in-memory fs
@@ -22,111 +27,43 @@ describe("testing fs mocking", () => {
     vi.resetAllMocks();
   });
 
-  test("data written directly", async () => {
-    //data is a multiline string, written directly to the path
-    const path = "/valid.csv";
-    const data = `Time,X,Y
-"2025-01-18",10,15
-"2025-01-19",20,25
-"2025-01-20",30,35
-"2025-01-21",40,45`;
-
+  test("writeFileSync writes data correctly", () => {
     fs.writeFileSync(path, data);
     //assert that mock writeFile was called
-    expect(fs.writeFileSync).toBeCalledTimes(1);
-    const localReaderPromise: Promise<Record<string, string | number>[]> =
-      localReader(path);
-    //assert that mock promise.readFile was called
-    expect(fsPromise.readFile).toBeCalledTimes(1);
-    //assert that promised data is defined
-    await expect(localReaderPromise).resolves.toBeDefined();
-    const localReaderData: Record<string, string | number>[] =
-      await localReaderPromise;
-    //each line MUST have Time, X and Y in order
-    localReaderData.forEach((line: Record<string, string | number>) => {
-      expect(Object.keys(line)).toEqual(["Time", "X", "Y"]);
-    });
+    expect(fs.writeFileSync).toHaveBeenCalledTimes(1);
+    expect(fs.writeFileSync).toHaveBeenCalledWith(path, data);
 
-    const localHeadersData: string[] = Object.keys(localReaderData[0]);
-    //assert that headers have Time,X,Y in order
-    expect(localHeadersData).toEqual(["Time", "X", "Y"]);
+    const writtenData = memVol.readFileSync(path, 'utf8');
+    expect(writtenData).toBe(data)
   });
 
-  test("translating Record<string,string | number>[] to and from", async () => {
-    //defining original Record<string,string | number>[] first, to compare to the data received from localReader
-    const path = "/fromTSD.csv";
+  test("readFileSync reads data correctly", () => {
+    memVol.writeFileSync(path, data);
 
-    const tsd: Record<string, string | number>[] = [];
-    const line1: Record<string, string | number> = {
-      Time: "2025-01-01",
-      X: 10,
-      Y: 20,
-    };
-    const line2: Record<string, string | number> = {
-      Time: "2025-01-03",
-      X: 12,
-      Y: 25,
-    };
-    const line3: Record<string, string | number> = {
-      Time: "2025-01-05",
-      X: 14,
-      Y: 30,
-    };
-    const line4: Record<string, string | number> = {
-      Time: "2025-01-07",
-      X: 16,
-      Y: 35,
-    };
-    tsd.push(line1, line2, line3, line4);
+    const writtenData = fs.readFileSync(path, 'utf8');
+    expect(fs.readFileSync).toHaveBeenCalledTimes(1);
+    expect(fs.readFileSync).toHaveBeenCalledWith(path, 'utf8');
 
-    let writeFileString = "";
-
-    //defining CSVHeader
-    const csvheaders: string[] = Object.keys(line1).map(String);
-    const headersString: string = csvheaders.toString();
-    writeFileString += headersString + "\n";
-
-    tsd.forEach((line: Record<string, string | number>, index: number) => {
-      const writeLine = Object.values(line).toString();
-      writeFileString += writeLine;
-      if (index != tsd.length - 1) {
-        writeFileString += "\n";
-      }
-    });
-
-    fs.writeFileSync(path, writeFileString);
-    //assert that mock writeFile was called
-    expect(fs.writeFileSync).toBeCalledTimes(1);
-
-    const localReaderPromise = localReader(path);
-    //assert that mock promise.readFile was called
-    expect(fsPromise.readFile).toBeCalledTimes(1);
-    //assert that promised data is defined
-    await expect(localReaderPromise).resolves.toBeDefined();
-    const localReaderData: Record<string, string | number>[] =
-      await localReaderPromise;
-    //map each line to line.key cause that is what holds {Time,X,Y}
-    const records = Object.values(tsd);
-    localReaderData.forEach((line: Record<string, string | number>) => {
-      //assert that each line has correct keys
-      expect(Object.keys(line).sort()).toEqual(["Time", "X", "Y"].sort());
-      //assert that each line has correct data types
-      expect(line).toEqual(
-        expect.objectContaining({
-          Time: expect.any(String) as string,
-          X: expect.any(Number) as number,
-          Y: expect.any(Number) as number,
-        }),
-      );
-    });
-    //assert that each line.key equals localReaderData
-    expect(records).toEqual(localReaderData);
-
-    const localHeadersData: string[] = Object.keys(localReaderData[0]);
-    //assert that returned headers are the same as original
-    expect(localHeadersData).toEqual(csvheaders);
+    expect(writtenData).toBe(data);
   });
-}).catch((err: unknown) => {
-  console.error(err as Error);
-  throw err as Error;
+
+  test("writeFile writes data correctly", async() => {
+    await fsPromise.writeFile(path, data);
+
+    expect(fsPromise.writeFile).toHaveBeenCalledTimes(1);
+    expect(fsPromise.writeFile).toHaveBeenCalledWith(path, data);
+
+    const writtenData = await memVol.promises.readFile(path, "utf8");
+    expect(writtenData).toBe(data)
+  });
+
+  test("readFile reads data correctly", async() => {
+    await memVol.promises.writeFile(path, data);
+
+    const writtenData = await fsPromise.readFile(path, "utf8");
+    expect(fsPromise.readFile).toHaveBeenCalledTimes(1);
+    expect(fsPromise.readFile).toHaveBeenCalledWith(path, "utf8");
+
+    expect(writtenData).toBe(data);
+  });
 });

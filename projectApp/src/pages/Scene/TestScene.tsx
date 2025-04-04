273639106fb40b717614264f93411af449072667
ona341 <aaghaizu@yahoo.com>
2025-04-03 20:52:06 -0600

    Update TestScene.tsx

--- projectApp/src/pages/Scene/TestScene.tsx
+++ projectApp/src/pages/Scene/TestScene.tsx
@@ -72,6 +72,7 @@ export default function TestScene({
             <Text fontSize={12} color={"white"}>
               Test Scene
             </Text>
+            
           </Container>
         </Container>
 
